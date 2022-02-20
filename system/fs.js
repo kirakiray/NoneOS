e2a5726da13d956b25e68cf88fe614cd5dca368d
YAO <kirayao@hotmail.com>
2022-02-20 21:32:35 +0800

    feat: add mkdir

--- system/fs.js
+++ system/fs.js
@@ -1,6 +1,6 @@
 (async () => {
   // 获取随机id
-  const getUid = (() => {
+  const createFid = (() => {
     if (window.crypto && crypto.randomUUID) {
       return crypto.randomUUID.bind(crypto);
     }
@@ -9,6 +9,23 @@
         .map((e) => e.toString(16))
         .join("");
   })();
+
+  // 没有初始化的情况下，进行根目录初始化
+  const initRoot = async () => {
+    const rootInfo = await readDB("/");
+
+    // root初始化
+    if (!rootInfo) {
+      await writeDB([
+        {
+          fid: "/",
+          name: "/",
+          type: "folder",
+          content: {},
+        },
+      ]);
+    }
+  };
 
   const filedb = new Promise((resolve, reject) => {
     // 根据id获取数据库
@@ -27,7 +44,8 @@
       let db = e.target.result;
 
       // 为该数据库创建一个对象仓库
-      db.createObjectStore("main", { keyPath: "fid" });
+      db.createObjectStore("files", { keyPath: "fid" });
+      // db.createObjectStore("map", { path: "fid" });
     };
 
     req.onerror = (event) => {
@@ -38,27 +56,50 @@
     };
   });
 
-  // 写入文件
-  const write = async (opts) => {
-    // const defs = {
-    //     // 路径
-    //     path: "",
-    //     // 类型
-    //     type: "file", // folder 文件夹类型；
-    //     // 保存内容
-    //     content: ""
+  // 添加文件或文件夹
+  const writeDB = async (opts) => {
+    // const item = {
+    //   // 类型
+    //   type: opts.type, // file 文件类型；folder 文件夹类型；
+    //   // 保存内容
+    //   content: [],
+    //   // 文件专属id
+    //   fid: opts.fid,
     // };
+    const db = await filedb;
 
+    const objectStore = db
+      .transaction(["files"], "readwrite")
+      .objectStore("files");
+
+    return Promise.all(
+      opts.map(
+        (data) =>
+          new Promise((resolve, reject) => {
+            const req = objectStore.put(data);
+            req.onsuccess = (e) => {
+              resolve(true);
+            };
+            req.onerror = (err) => {
+              reject(err);
+            };
+          })
+      )
+    );
+  };
+
+  // 读取文件或文件夹
+  const readDB = async (fid) => {
     const db = await filedb;
 
     return new Promise((resolve, reject) => {
       let req = db
-        .transaction(["main"], "readwrite")
-        .objectStore("main")
-        .put(opts);
+        .transaction(["files"], "readonly")
+        .objectStore("files")
+        .get(fid);
 
       req.onsuccess = (e) => {
-        resolve(true);
+        resolve(e.target.result);
       };
       req.onerror = (err) => {
         reject(err);
@@ -66,8 +107,71 @@
     });
   };
 
-  const fs = { write };
+  const getPathArr = (path) => {
+    const pathArr = path.split("/");
+
+    // 去除第一个根目录空白
+    if (!pathArr[0]) {
+      pathArr.splice(0, 1);
+    }
+
+    return pathArr;
+  };
+
+  // 添加文件夹
+  const mkdir = async (path) => {
+    const pathArr = getPathArr(path),
+      lastIndex = pathArr.length - 1;
+    let index = 0;
+
+    let parentDirData = await readDB("/");
+
+    while (index <= lastIndex) {
+      const name = pathArr[index];
+      if (index === lastIndex) {
+        const { content } = parentDirData;
+
+        // 确定没有重复
+        if (content[name]) {
+          throw `${path} already exists`;
+        }
+
+        // 写入文件夹
+        const fid = createFid();
+        const subDir = {
+          fid,
+          name,
+          content: {},
+        };
+
+        content[name] = {
+          type: "folder",
+          fid,
+          name,
+        };
+
+        await writeDB([subDir, parentDirData]);
+      } else {
+        const subData = parentDirData.content[name];
+        if (!subData) {
+          throw `can not read ${path}`;
+        }
+        parentDirData = await readDB(subData.fid);
+      }
+      index++;
+    }
+
+    return true;
+  };
+
+  const rename = async (opts) => {};
+
+  const remove = async (opts) => {};
+
+  const fs = {
+    mkdir,
+    inited: initRoot(),
+  };
 
   window.fs = fs;
-  window.getUid = getUid;
 })();
