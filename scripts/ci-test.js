9078d97530c3e5b50fe2a613ffe8545fa9915b08
huang yao <kirayao@hotmail.com>
2023-03-01 00:47:57 +0800

    debug ci

--- scripts/ci-test.js
+++ scripts/ci-test.js
@@ -1,23 +1,34 @@
 const server = require("./server");
-const { exec } = require("child_process");
+const shell = require("shelljs");
 
 console.log("dirname => ", __dirname);
 
 // 运行命令行命令
-exec(
-  `cd ${__dirname}
-pwd
-npm run test
-`,
-  (error, stdout, stderr) => {
-    if (error) {
-      setTimeout(() => {
-        server.close();
-      }, 0);
-      throw error;
-    }
-    console.log(`stdout: ${stdout}`);
-    console.error(`stderr: ${stderr}`);
-    server.close();
+shell.exec(`npm run test`, function (code, stdout, stderr) {
+  console.log("Exit code:", code);
+  console.log("Program output:", stdout);
+  console.log("Program stderr:", stderr);
+  if (code === 0) {
+    console.log("成功");
   }
-);
+  server.close();
+});
+
+// 运行命令行命令
+// exec(
+//   `cd ${__dirname}
+// pwd
+// npm run test
+// `,
+//   (error, stdout, stderr) => {
+//     if (error) {
+//       setTimeout(() => {
+//         server.close();
+//       }, 0);
+//       throw error;
+//     }
+//     console.log(`stdout: ${stdout}`);
+//     console.error(`stderr: ${stderr}`);
+//     server.close();
+//   }
+// );
