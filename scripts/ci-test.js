315fcfc83367c0ac795ed069b69c3a8f74f9f8ef
huang yao <kirayao@hotmail.com>
2023-02-28 23:23:05 +0800

    debug ci

--- scripts/ci-test.js
+++ scripts/ci-test.js
@@ -5,6 +5,7 @@ const { exec } = require("child_process");
 exec("npm run test", (error, stdout, stderr) => {
   if (error) {
     console.error(`exec error: ${error}`);
+    server.close();
     return;
   }
   console.log(`stdout: ${stdout}`);
