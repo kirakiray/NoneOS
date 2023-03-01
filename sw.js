f4af24d6c5874dd71fb34052dc01b0fc56e9c064
huangyao <kirayao@hotmail.com>
2023-03-01 22:44:38 +0800

    feat: Let requests dock to the file system (#3)
    * feat: add proxy mode
    
    * refactor: change register service file name
    
    * fi: sw support firefox
    
    * fix: test case in firefox

---
+++ sw.js
@@ -0,0 +1,44 @@
+import fs from "./packages/fs/index.mjs";
+
+self.addEventListener("fetch", function (event) {
+  const { request } = event;
+
+  const urldata = new URL(request.url);
+
+  if (/^\/@/.test(urldata.pathname)) {
+    const path = urldata.pathname.replace(/^\/@/, "");
+
+    event.respondWith(
+      (async () => {
+        try {
+          const data = await fs.readFile(path);
+
+          if (!data) {
+            throw `File does not exist : ${urldata.pathname}`;
+          }
+
+          return new Response(data, {
+            status: 200,
+          });
+        } catch (err) {
+          console.error(err);
+          return new Response(undefined, {
+            status: 404,
+          });
+        }
+      })()
+    );
+  }
+
+  console.log("urldata => ", urldata);
+});
+
+self.addEventListener("install", () => {
+  self.skipWaiting();
+
+  console.log("NoneOS installation successful");
+});
+
+self.addEventListener("activate", () => {
+  console.log("NoneOS server activation successful");
+});
