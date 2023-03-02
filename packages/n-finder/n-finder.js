83eaa315dfd78fc2079acaa90262425beffca400
huang yao <kirayao@hotmail.com>
2023-03-02 23:01:51 +0800

    backup n-finder code

---
+++ packages/n-finder/n-finder.js
@@ -0,0 +1,13 @@
+Component(async ({ load }) => {
+  return {
+    data: {
+      ha: "asdasd",
+      files: [
+        {
+          name: "hah",
+        },
+        { name: "222" },
+      ],
+    },
+  };
+});
