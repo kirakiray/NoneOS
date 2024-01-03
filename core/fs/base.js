15f086d2082646ca5ee42f70397197b9b357f939
huang yao <5945154+kirakiray@users.noreply.github.com>
2024-01-03 19:37:27 +0800

    fix: Re-split the files of the file system

--- core/fs/base.js
+++ core/fs/base.js
@@ -1,14 +1,14 @@
+// 替换这个基础库，理论是可以兼容各个环境
 export class NBaseHandle {
+  #root;
   constructor(handle, paths, root) {
+    this.#root = root || this;
     Object.defineProperties(this, {
       relativePaths: {
         value: paths || [],
       },
       _handle: {
         value: handle,
-      },
-      _root: {
-        value: root || this,
       },
     });
   }
@@ -17,10 +17,20 @@ export class NBaseHandle {
     return this._handle?.kind || "directory";
   }
 
-  get parent() {}
+  get parent() {
+    if (!this.relativePaths.length) {
+      return null;
+    }
+
+    debugger;
+  }
 
   get path() {
     return this.relativePaths.join("/");
+  }
+
+  get root() {
+    return this.#root;
   }
 
   get name() {
@@ -38,180 +48,9 @@ export class NBaseHandle {
     };
 
     Object.assign(defaults, options);
+
+    debugger;
   }
 
   async move() {}
 }
-
-export class NDirHandle extends NBaseHandle {
-  constructor(handle, paths, root) {
-    super(handle, paths, root);
-  }
-
-  async read(name, options) {
-    const defaults = {
-      //   type: "file",
-      create: false,
-    };
-
-    Object.assign(defaults, options);
-
-    const paths = name.split("/");
-    const lastId = paths.length - 1;
-
-    let targetHandle = this._handle;
-    let count = 0;
-
-    for (let name of paths) {
-      if (count === lastId) {
-        if (defaults.type) {
-          try {
-            switch (defaults.type) {
-              case "file":
-                targetHandle = await targetHandle.getFileHandle(name, {
-                  create: defaults.create,
-                });
-                break;
-              case "directory":
-                targetHandle = await targetHandle.getDirectoryHandle(name, {
-                  create: defaults.create,
-                });
-                break;
-            }
-          } catch (err) {
-            console.error(err);
-            return err;
-          }
-        } else {
-          let lastHandle;
-          try {
-            lastHandle = await targetHandle.getDirectoryHandle(name);
-          } catch (err) {
-            try {
-              lastHandle = await targetHandle.getFileHandle(name, {
-                create: defaults.create,
-              });
-            } catch (err2) {
-              console.error(err2);
-              return err2;
-            }
-          }
-
-          targetHandle = lastHandle;
-        }
-        break;
-      }
-
-      targetHandle = await targetHandle.getDirectoryHandle(name, {
-        create: defaults.create,
-      });
-
-      count++;
-    }
-
-    if (targetHandle.kind === "file") {
-      return new NFileHandle(targetHandle, paths, this._root);
-    } else if (targetHandle.kind === "directory") {
-      return new NDirHandle(targetHandle, paths, this._root);
-    }
-
-    debugger;
-
-    return null;
-  }
-
-  async *entries() {
-    for await (let [name, handle] of this._handle.entries()) {
-      if (handle.kind === "file") {
-        yield new NFileHandle(
-          handle,
-          [...this.relativePaths, name],
-          this._root
-        );
-      } else {
-        yield new NDirHandle(handle, [...this.relativePaths, name], this._root);
-      }
-    }
-  }
-}
-
-export class NFileHandle extends NBaseHandle {
-  constructor(handle, paths, root) {
-    super(handle, paths, root);
-  }
-
-  async write(content, options) {
-    if (this.kind === "directory") {
-      throw new Error(`Directory cannot use write method`);
-    }
-  }
-
-  async _get(options) {
-    const defaults = {
-      type: "file", // text buffer
-    };
-
-    Object.assign(defaults, options);
-
-    const file = await this._handle.getFile();
-
-    if (defaults.type === "file") {
-      return file;
-    }
-
-    return new Promise((resolve, reject) => {
-      const reader = new FileReader();
-
-      reader.onload = () => resolve(reader.result);
-
-      switch (defaults.type) {
-        case "text":
-          reader.readAsText(file);
-          break;
-        case "buffer":
-          reader.readAsArrayBuffer(file);
-          break;
-        default:
-          reject(new Error(`"${defaults.type}" type is not supported`));
-      }
-    });
-  }
-
-  file() {
-    return this._get({ type: "file" });
-  }
-
-  text() {
-    return this._get({ type: "text" });
-  }
-
-  buffer() {
-    return this._get({ type: "buffer" });
-  }
-
-  async stat() {
-    const file = await this._get({ type: "file" });
-
-    const result = {};
-
-    ["lastModified", "lastModifiedDate", "name", "size", "type"].forEach(
-      (k) => (result[k] = file[k])
-    );
-
-    return result;
-  }
-}
-
-const rootHandlePms = navigator.storage.getDirectory();
-
-export const read = async (path = "", { handle, create, type } = {}) => {
-  const root = new NDirHandle(handle || (await rootHandlePms));
-  if (!path) {
-    return root;
-  }
-
-  return await root.read(path, {
-    create,
-    type,
-  });
-};
---
+++ core/fs/main.js
@@ -0,0 +1,176 @@
+import { NBaseHandle } from "./base.js";
+
+export class NDirHandle extends NBaseHandle {
+  constructor(handle, paths, root) {
+    super(handle, paths, root);
+  }
+
+  async get(name, options) {
+    const defaults = {
+      //   type: "file",
+      create: false,
+    };
+
+    Object.assign(defaults, options);
+
+    const paths = name.split("/");
+    const lastId = paths.length - 1;
+
+    let targetHandle = this._handle;
+    let count = 0;
+
+    for (let name of paths) {
+      if (count === lastId) {
+        if (defaults.type) {
+          try {
+            switch (defaults.type) {
+              case "file":
+                targetHandle = await targetHandle.getFileHandle(name, {
+                  create: defaults.create,
+                });
+                break;
+              case "directory":
+                targetHandle = await targetHandle.getDirectoryHandle(name, {
+                  create: defaults.create,
+                });
+                break;
+            }
+          } catch (err) {
+            console.error(err);
+            return err;
+          }
+        } else {
+          let lastHandle;
+          try {
+            lastHandle = await targetHandle.getDirectoryHandle(name);
+          } catch (err) {
+            try {
+              lastHandle = await targetHandle.getFileHandle(name, {
+                create: defaults.create,
+              });
+            } catch (err2) {
+              console.error(err2);
+              return err2;
+            }
+          }
+
+          targetHandle = lastHandle;
+        }
+        break;
+      }
+
+      targetHandle = await targetHandle.getDirectoryHandle(name, {
+        create: defaults.create,
+      });
+
+      count++;
+    }
+
+    if (targetHandle.kind === "file") {
+      return new NFileHandle(targetHandle, paths, this.root);
+    } else if (targetHandle.kind === "directory") {
+      return new NDirHandle(targetHandle, paths, this.root);
+    }
+
+    return null;
+  }
+
+  async *entries() {
+    for await (let [name, handle] of this._handle.entries()) {
+      if (handle.kind === "file") {
+        yield new NFileHandle(handle, [...this.relativePaths, name], this.root);
+      } else {
+        yield new NDirHandle(handle, [...this.relativePaths, name], this.root);
+      }
+    }
+  }
+}
+
+export class NFileHandle extends NBaseHandle {
+  constructor(handle, paths, root) {
+    super(handle, paths, root);
+  }
+
+  async write(content) {
+    const writable = await this.getWriter();
+    await writable.write(content);
+    await writable.close();
+  }
+
+  getWriter() {
+    return this._handle.createWritable();
+  }
+
+  async read(options) {
+    const defaults = {
+      type: "file", // text buffer
+    };
+
+    Object.assign(defaults, options);
+
+    const file = await this._handle.getFile();
+
+    if (defaults.type === "file") {
+      return file;
+    }
+
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+
+      reader.onload = () => resolve(reader.result);
+
+      switch (defaults.type) {
+        case "text":
+          reader.readAsText(file);
+          break;
+        case "buffer":
+          reader.readAsArrayBuffer(file);
+          break;
+        default:
+          reject(new Error(`"${defaults.type}" type is not supported`));
+      }
+    });
+  }
+
+  file() {
+    return this.read({ type: "file" });
+  }
+
+  text() {
+    return this.read({ type: "text" });
+  }
+
+  buffer() {
+    return this.read({ type: "buffer" });
+  }
+
+  async stat() {
+    const file = await this.read({ type: "file" });
+
+    const result = {};
+
+    ["lastModified", "lastModifiedDate", "name", "size", "type"].forEach(
+      (k) => (result[k] = file[k])
+    );
+
+    return result;
+  }
+}
+
+const rootHandlePms = navigator.storage.getDirectory();
+
+// 获取本地的 file system 数据
+export const get = async (path = "", { handle, create, type } = {}) => {
+  const root = new NDirHandle(handle || (await rootHandlePms));
+  if (!path) {
+    return root;
+  }
+
+  return await root.get(path, {
+    create,
+    type,
+  });
+};
+
+// 通过打开浏览器选择目录
+export const open = async () => {};
