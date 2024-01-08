19adc8cabea772ff74849c6978daf584b707063a
huang yao <5945154+kirakiray@users.noreply.github.com>
2024-01-08 15:27:43 +0800

    fix: some issuse

--- core/apps/finder/app-config.js
+++ core/apps/finder/app-config.js
@@ -1,9 +1,18 @@
 export const home = "./files.html";
 
 export const loading = () => {
-  return `<div style="display:flex;justify-content:center;align-items:center;">
-    Loading
-  </div>`;
+  const loadingEl = $({
+    tag: "div",
+    css: {
+      width: "100%",
+      height: "100%",
+      position: "absolute",
+      zIndex: 1000,
+    },
+    html: `
+      <div style="transition: all 10s cubic-bezier(0, 0, 0.22, 0.84) 0s; height: 2px;width: 0;background-color: rgb(0, 161, 46);"></div>
+    `,
+  });
 };
 
 export const pageAnime = {
