fbf76bb4291711e0d2d4937c64ff766c13a722b7
huang yao <5945154+kirakiray@users.noreply.github.com>
2024-01-14 15:27:07 +0800

    feat: The contents of the hard drive can be mounted

--- core/fs/main.js
+++ core/fs/main.js
@@ -23,7 +23,7 @@ export class NDirHandle extends NBaseHandle {
       if (name === "node_modules") {
         continue;
       }
-      
+
       if (count === lastId) {
         if (defaults.type) {
           try {
@@ -194,6 +194,10 @@ export class NFileHandle extends NBaseHandle {
 
 const rootHandlePms = navigator.storage.getDirectory();
 
+export const getLocal = async () => {
+  return new NDirHandle(await rootHandlePms);
+};
+
 // 获取本地的 file system 数据
 export const get = async (path = "", { handle, create, type } = {}) => {
   const root = new NDirHandle(handle || (await rootHandlePms));
@@ -207,5 +211,35 @@ export const get = async (path = "", { handle, create, type } = {}) => {
   });
 };
 
+const otherHandles = [];
+
 // 通过打开浏览器选择目录
-export const open = async () => {};
+export const open = async () => {
+  if (window.showDirectoryPicker) {
+    const oriHandle = await window.showDirectoryPicker({});
+
+    const handle = new NDirHandle(oriHandle, []);
+
+    otherHandles.push({
+      name: handle.name,
+      handle,
+    });
+
+    return handle;
+  }
+
+  throw "showDirectoryPicker does not exist";
+};
+
+// 获取所有已经挂载的目录
+export const getAll = async () => {
+  const root = await getLocal();
+
+  return [
+    {
+      name: "Local",
+      handle: root,
+    },
+    ...otherHandles,
+  ];
+};
