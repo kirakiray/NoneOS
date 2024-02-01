7aa54ecd5feff7bb858c17947b387641a6415e22
huang yao <5945154+kirakiray@users.noreply.github.com>
2024-02-01 14:19:07 +0800

    feat: add apps data

---
+++ core/data.js
@@ -0,0 +1,22 @@
+export const systemApps = $.stanz([
+  {
+    isHome: true,
+    path: "/core/apps/finder/app-config.js",
+  },
+  {
+    path: "/core/apps/editor/app-config.js",
+  },
+]);
+
+const selfPath = import.meta.url;
+
+// 填充应用数据
+systemApps.forEach(async (item) => {
+  const appUrlObj = new URL(item.path, selfPath);
+  const homeAppData = await import(appUrlObj.href);
+
+  item.icon = new URL(homeAppData.icon, appUrlObj).href;
+  item.name = homeAppData.name;
+});
+
+export const installedApps = $.stanz([]);
