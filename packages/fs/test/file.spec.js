1b3075e246df9fd3ad4462ae3bb077dcde62442e
huang yao <5945154+kirakiray@users.noreply.github.com>
2024-06-26 20:48:21 +0800

    test: add playwright test

---
+++ packages/fs/test/file.spec.js
@@ -0,0 +1,14 @@
+import { test, expect } from "@playwright/test";
+
+test("file system test", async ({ page }) => {
+  await page.goto("http://127.0.0.1:5559/packages/fs/test/test-fs.html");
+  await page.getByText("get local ok").click();
+  await page.getByText("get subDir ok").click();
+  await page.getByText("multi handle Id ok").click();
+  await page.getByText("dir path ok").click();
+  await page.getByText("file id ok").click();
+  await page.getByText("read file content ok").click();
+  await page.getByText("long text ok").click();
+  await page.getByText("read range ok").click();
+  await page.getByText("read random range ok").click();
+});
