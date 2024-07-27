74814ea6c80536cad6b439f977c26d3a76ee7dbf
huang yao <5945154+kirakiray@users.noreply.github.com>
2024-07-27 22:37:13 +0800

    feat: add export

---
+++ packages/fs/zip/main.js
@@ -0,0 +1,98 @@
+let worker;
+const workerPath = import.meta.resolve("./worker.js");
+
+try {
+  worker = new Worker(workerPath);
+} catch (err) {
+  const workerBlob = await fetch(workerPath).then((e) => e.blob());
+  const workerUrl = URL.createObjectURL(workerBlob);
+  worker = new Worker(workerUrl);
+}
+
+const getTaskId = () => Math.random().toString("16").slice(2);
+
+const resolver = {};
+
+worker.onmessage = (e) => {
+  if (e.data.error) {
+    resolver[e.data.id].reject(e.data.error);
+  } else {
+    resolver[e.data.id].resolve(e.data.content);
+  }
+  resolver[e.data.id] = null;
+};
+
+export async function unzip(file) {
+  const taskID = getTaskId();
+
+  worker.postMessage({
+    taskType: "unzip",
+    id: taskID,
+    file,
+  });
+
+  const files = await new Promise((resolve, reject) => {
+    resolver[taskID] = { resolve, reject };
+  });
+
+  return files;
+}
+
+// 导入文件夹压缩包数据
+export async function exploreFolder(handler, rootName) {
+  const taskID = getTaskId();
+
+  const files = await flatFiles(handler);
+  let len = files.length;
+
+  const reobj = new Promise((resolve, reject) => {
+    resolver[taskID] = { resolve, reject };
+  });
+
+  for (let e of files) {
+    const { handle } = e;
+    len--;
+
+    const file = await handle.file();
+
+    worker.postMessage({
+      id: taskID,
+      path: `${rootName}/${
+        e.parentsName.length ? e.parentsName.join("/") + "/" : ""
+      }${e.name}`,
+      file,
+      isEnd: !len,
+    });
+  }
+
+  return reobj;
+}
+
+export async function flatFiles(parHandle, parentsName = []) {
+  const files = [];
+  let isEmpty = true;
+
+  for await (let [name, handle] of parHandle.entries()) {
+    isEmpty = false;
+    if (handle.kind === "file") {
+      files.push({
+        kind: "file",
+        name,
+        handle,
+        parentsName,
+      });
+    } else {
+      const subFiles = await flatFiles(handle, [...parentsName, name]);
+      files.push(...subFiles);
+    }
+  }
+
+  if (isEmpty) {
+    files.push({
+      kind: "dir",
+      parentsName,
+    });
+  }
+
+  return files;
+}
