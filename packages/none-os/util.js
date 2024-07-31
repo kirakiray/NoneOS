5a0caaeb8419d05ed9f321cc1a36d9c05c5efb70
huang yao <5945154+kirakiray@users.noreply.github.com>
2024-07-31 23:27:45 +0800

    feat: add update version

--- packages/none-os/util.js
+++ packages/none-os/util.js
@@ -2,17 +2,25 @@ import { unzip } from "../zip/main.js";
 import { get } from "../fs/main.js";
 
 // 安装系统依赖文件
-export const installOS = async () => {
+export const installOS = async (callback) => {
   const zipFile = await fetch("/packages.zip").then((e) => e.blob());
   const files = await unzip(zipFile);
 
   const packagesHandle = await get("packages");
+
+  const { length } = files;
+  let count = 0;
 
   for (let { path, file } of files) {
     const fileHandle = await packagesHandle.get(path, {
       create: "file",
     });
     await fileHandle.write(file);
+    count++;
+
+    if (length) {
+      await callback({ count, length });
+    }
   }
 
   return true;
