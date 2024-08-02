0da60e9e8ffc7875d46a11f09092c6ee7d00eef2
huang yao <5945154+kirakiray@users.noreply.github.com>
2024-08-02 11:22:36 +0800

    feat: add re-install

--- packages/none-os/util.js
+++ packages/none-os/util.js
@@ -2,15 +2,18 @@ import { unzip } from "../zip/main.js";
 import { get } from "../fs/main.js";
 
 // 安装系统依赖文件
-export const installOS = async (callback) => {
+export const installOS = async (callback, packagesUrl = "/packages.zip") => {
   // 缓存 packages
-  const zipFile = await fetch("/packages.zip").then((e) => e.blob());
+  const zipFile = await fetch(packagesUrl).then((e) => e.blob());
   const files = await unzip(zipFile);
 
   const packagesHandle = await get("packages");
 
   const { length } = files;
   let count = 0;
+
+  // 标识安装中
+  localStorage.__installing = 1;
 
   for (let { path, file } of files) {
     const fileHandle = await packagesHandle.get(path, {
@@ -23,6 +26,8 @@ export const installOS = async (callback) => {
       await callback({ count, length });
     }
   }
+
+  localStorage.removeItem("__installing");
 
   return true;
 };
