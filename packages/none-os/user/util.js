7ee27b2e2a4ade88027566a116518ff42374c4ef
huang yao <5945154+kirakiray@users.noreply.github.com>
2024-08-04 21:28:34 +0800

    feat: add default pair

---
+++ packages/none-os/user/util.js
@@ -0,0 +1,48 @@
+// 生成签名用公钥和私钥
+export async function generateSignKeyPair() {
+  return await crypto.subtle.generateKey(
+    {
+      name: "RSA-PSS",
+      modulusLength: 2048, //密钥长度，可以是1024, 2048, 4096，建议2048以上
+      publicExponent: new Uint8Array([0x01, 0x00, 0x01]), // 公共指数e，一般用65537
+      hash: "SHA-256", //可以是"SHA-1", "SHA-256", "SHA-384", "SHA-512"
+    },
+    true,
+    ["sign", "verify"]
+  );
+}
+
+// 生成非对称加密用公钥和私钥
+export async function generateEncryKeyPair() {
+  return await crypto.subtle.generateKey(
+    {
+      name: "RSA-OAEP",
+      modulusLength: 2048, //密钥长度，可以是1024, 2048, 4096，建议2048以上
+      publicExponent: new Uint8Array([0x01, 0x00, 0x01]), // 公共指数e，一般用65537
+      hash: "SHA-256", //可以是"SHA-1", "SHA-256", "SHA-384", "SHA-512"
+    },
+    true,
+    ["encrypt", "decrypt"]
+  );
+}
+
+// ArrayBuffer 转 Base64 字符串
+export function arrayBufferToBase64(buffer) {
+  const uint8Array = new Uint8Array(buffer);
+  let binary = "";
+  for (let i = 0; i < uint8Array.length; i++) {
+    binary += String.fromCharCode(uint8Array[i]);
+  }
+  return btoa(binary);
+}
+
+// Base64 字符串转 ArrayBuffer
+export function base64ToArrayBuffer(base64String) {
+  const binaryString = atob(base64String);
+  const length = binaryString.length;
+  const uint8Array = new Uint8Array(length);
+  for (let i = 0; i < length; i++) {
+    uint8Array[i] = binaryString.charCodeAt(i);
+  }
+  return uint8Array.buffer;
+}
