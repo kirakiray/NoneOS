f32464c08b3a66a1b75e637e17232952011019ab
huang yao <5945154+kirakiray@users.noreply.github.com>
2024-08-10 00:34:32 +0800

    refactor: backup code

---
+++ packages/none-os/user/client-user.js
@@ -0,0 +1,26 @@
+import { User } from "./public-user.js";
+
+export class ClientUser extends User {
+  constructor(...args) {
+    super(...args);
+  }
+
+  // 发送数据给服务端
+  async post(data) {
+    debugger;
+  }
+
+  // 连接用户
+  async connect() {
+    debugger;
+  }
+
+  // 初始化通道
+  async init(server) {
+    this._server = server;
+  }
+
+  async close() {
+    debugger;
+  }
+}
