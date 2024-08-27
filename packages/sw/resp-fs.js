e191ac0195200e5dab57d125e0f4f2e34e3475b0
huang yao <5945154+kirakiray@users.noreply.github.com>
2024-08-28 00:09:40 +0800

    fix: app entry

--- packages/sw/resp-fs.js
+++ packages/sw/resp-fs.js
@@ -12,9 +12,9 @@ const resposeFS = async ({ request }) => {
   if (
     pathArr.length === 3 &&
     (pathArr[0] === "apps" || pathArr[0] === "packages") &&
-    pathArr[2] === "app"
+    (pathArr[2] === "app" || pathArr[2] === "appdebug")
   ) {
-    return resposeApp({ pathname });
+    return resposeApp({ pathname, path });
   }
 
   // console.log("path:", path);
@@ -43,17 +43,35 @@ const resposeFS = async ({ request }) => {
 };
 
 // 以app入口的形式返回内容
-const resposeApp = async ({ pathname }) => {
+const resposeApp = async ({ pathname, path }) => {
+  let appconfig;
+
+  // 获取父路径
+  const pathArr = path.split("/");
+  const parentPath = pathArr.slice(0, -1).join("/");
+
+  const isdebug = pathArr.slice(-1)[0] === "appdebug";
+
+  try {
+    appconfig = await get(`${parentPath}/app.json`);
+    appconfig = JSON.parse(await appconfig.text());
+  } catch (err) {
+    appconfig = await fetch(`/${parentPath}/app.json`).then((e) => e.json());
+  }
+
+  console.log("appconfig: ", appconfig);
 
   return new Response(
     `<!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Setting App</title>
-    <script src="/packages/ofa/ofa.js" debug></script>
-    <script src="/packages/ofa/router.min.js" debug></script>
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+    <title>${appconfig.name}</title>
+    <link rel="shortcut icon" href="${appconfig.icon}">
+    <script src="/packages/ofa/ofa.js"${isdebug ? " debug" : ""}></script>
+    <script src="/packages/ofa/router.min.js"></script>
     <script src="/packages/pui/init.js" type="module"></script>
     <style>
       html,
@@ -70,7 +88,7 @@ const resposeApp = async ({ pathname }) => {
   </head>
   <body>
     <o-router fix-body>
-      <o-app src="./app-config.js"></o-app>
+      <o-app src="${appconfig.config}"></o-app>
     </o-router>
     <o-root-provider name="pui" theme="dark"></o-root-provider>
     <o-root-provider name="clipboard" type="no"></o-root-provider>
