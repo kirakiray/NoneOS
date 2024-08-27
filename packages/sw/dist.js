a874bbadc342df403c702ea5a2a6320aa19529e5
huang yao <5945154+kirakiray@users.noreply.github.com>
2024-08-27 23:02:21 +0800

    fix: dark mode

--- packages/sw/dist.js
+++ packages/sw/dist.js
@@ -1283,10 +1283,21 @@
   const resposeFS = async ({ request }) => {
     const { pathname } = new URL(request.url);
 
-    const path = pathname.replace(/^\/\$\//, "");
+    const path = decodeURIComponent(pathname.replace(/^\/\$\//, ""));
+
+    // 判断是不是app入口
+    const pathArr = path.split("/");
+
+    if (
+      pathArr.length === 3 &&
+      (pathArr[0] === "apps" || pathArr[0] === "packages") &&
+      pathArr[2] === "app"
+    ) {
+      return resposeApp({ pathname });
+    }
 
     // console.log("path:", path);
-    const handle = await get(decodeURIComponent(path));
+    const handle = await get(path);
     let content = await handle.file();
 
     const headers = {};
@@ -1308,6 +1319,47 @@
       status: 200,
       headers,
     });
+  };
+
+  // 以app入口的形式返回内容
+  const resposeApp = async ({ pathname }) => {
+
+    return new Response(
+      `<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Setting App</title>
+    <script src="/packages/ofa.js" debug></script>
+    <script src="/packages/pui/init.js" type="module"></script>
+    <style>
+      html,
+      body {
+        margin: 0;
+        padding: 0;
+        height: 100%;
+      }
+
+      o-app {
+        height: 100%;
+      }
+    </style>
+  </head>
+  <body>
+    <o-app src="./app-config.js"></o-app>
+    <o-root-provider name="pui" theme="dark"></o-root-provider>
+    <o-root-provider name="clipboard" type="no"></o-root-provider>
+  </body>
+</html>
+    `,
+      {
+        status: 200,
+        headers: {
+          "Content-Type": "text/html; charset=utf-8",
+        },
+      }
+    );
   };
 
   /**
