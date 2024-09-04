c2d0b2eaf30ae3838d0ad54121988c94b30fb27f
huang yao <5945154+kirakiray@users.noreply.github.com>
2024-09-04 23:53:54 +0800

    feat: add remotes list

--- packages/fs/main.js
+++ packages/fs/main.js
@@ -1,6 +1,7 @@
 import get from "./get.js";
 import { get as originGet } from "./origin.js";
 export { showPicker } from "./origin.js";
+export { getRemotes } from "./remote.js";
 
 const origin = {
   get: originGet,
---
+++ packages/fs/remote.js
@@ -0,0 +1,24 @@
+import { getCerts } from "/packages/none-os/user/cert.js";
+import { getSelfUserInfo } from "/packages/none-os/user/main.js";
+import { getUserCard } from "/packages/none-os/user/usercard.js";
+
+// 获取可远端的根目录
+export const getRemotes = async () => {
+  const certs = await getCerts();
+  const selfUserInfo = await getSelfUserInfo();
+  const userCards = await getUserCard();
+
+  // 授权给自己的所有权证书
+  return certs
+    .filter((e) => {
+      return e.permission === "fully" && e.authTo === selfUserInfo.userID;
+    })
+    .map((certData) => {
+      const card = userCards.find((user) => user.id === certData.issuer);
+
+      return {
+        name: card?.name,
+        path: "$-remote-" + certData.issuer,
+      };
+    });
+};
