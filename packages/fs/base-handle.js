21369d748c1c2768b67ee6d9fee0911fe4da7e0e
huang yao <5945154+kirakiray@users.noreply.github.com>
2024-11-12 14:38:13 +0800

    backup cod

--- packages/fs/base-handle.js
+++ packages/fs/base-handle.js
@@ -9,6 +9,35 @@ export class PublicBaseHandle {
     return flatHandle(this);
   }
 
+  // 给拷贝进度用的方法，获取文件或文件夹的分块信息
+  async _info(options) {
+    const h128 = options?.h128;
+
+    const flatData = await this.flat();
+
+    const reData = await Promise.all(
+      flatData.map(async (item) => {
+        const { handle } = item;
+
+        const hashs1m = await handle._getHashs();
+
+        const valObj = {
+          size: item.size,
+          hashs1m,
+        };
+
+        if (h128) {
+          const hashs128k = await handle._getHashs({ chunkSize: 128 * 1024 });
+          valObj.hashs128k = hashs128k;
+        }
+
+        return [item.path, valObj];
+      })
+    );
+
+    return reData;
+  }
+
   // 按照需求将文件保存到缓存池中，方便远端获取
   async _saveCache({ options }) {
     // 获取指定的块内容
@@ -39,6 +68,7 @@ export class PublicBaseHandle {
     if (this.kind === "dir") {
       throw new Error(`The directory cannot use the hash method`);
     }
+
     const hashs = await this._getHashs();
 
     const hash = await calculateHash(hashs.join(""));
@@ -48,23 +78,14 @@ export class PublicBaseHandle {
 
   // 获取1mb分区哈希块数组
   async _getHashs(options) {
+    if (this.kind === "dir") {
+      throw new Error(`The directory cannot use the _getHashs method`);
+    }
+
     const chunkSize = options?.chunkSize || CHUNK_SIZE;
 
     if (this.kind === "file") {
       return getHashs(await this._fsh.getFile(), chunkSize);
     }
-
-    // 能进入这里，肯定是dir的类型
-    // 扁平文件数据
-    const flatdata = await this.flat();
-
-    // 获取所有hash
-    return await Promise.all(
-      flatdata.map(async (item) => {
-        const hashs = await item.handle._getHashs();
-
-        return [item.path, hashs];
-      })
-    );
   }
 }
--- packages/fs/task/main.js
+++ packages/fs/task/main.js
@@ -2,13 +2,40 @@
 export const copyTo = async (options) => {
   const { from: fHandle, to: tHandle } = options;
 
-  let flats;
+  // 复制到目的地的文件名
+  let finalName = options.name || fHandle.name;
 
-  // 如果是目录，获取所有文件
-  if (fHandle.kind === "dir") {
-    flats = await fHandle._getHashs();
-  } else {
-    flats = [[fHandle.path, await fHandle._getHashs()]];
+  // 获取扁平化的数据
+  const flatFileDatas = await fHandle._info();
+
+  {
+    // 修正来源目标的地址
+    const fromDirPath = fHandle.path;
+    const fixPath = (item) => {
+      const afterPath = item[0].replace(`${fromDirPath}/`, "");
+      item[0] = afterPath;
+    };
+    // 修正地址
+    flatFileDatas.forEach(fixPath);
+  }
+
+  if (fHandle.kind === "file") {
+    debugger;
+    return;
+  }
+
+  // 复制到这个目录
+  const targetDirHandle = await tHandle.get(finalName, {
+    create: "dir",
+  });
+
+  // 确认信息
+  if (options.confirm) {
+    const result = options.confirm(JSON.parse(JSON.stringify(flatFileDatas)));
+
+    if (!result) {
+      return;
+    }
   }
 
   debugger;
