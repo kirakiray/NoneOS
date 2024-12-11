59d1233d1989dacbcbf187337ebbe6da22ad55b6
huang yao <5945154+kirakiray@users.noreply.github.com>
2024-12-11 10:06:52 +0800

    fix: add verification

--- scripts/buhash.js
+++ scripts/buhash.js
@@ -1,5 +1,10 @@
 import fs from "fs/promises";
 import { getFilesAndHashes } from "./get-files-and-hashes.js";
+import {
+  stringToPair,
+  dataToArrayBuffer,
+  arrayBufferToBase64,
+} from "../packages/core/util.js";
 
 (async () => {
   try {
@@ -8,19 +13,53 @@ import { getFilesAndHashes } from "./get-files-and-hashes.js";
 
     const files = await getFilesAndHashes("./packages");
 
+    const data = {
+      files,
+      version: pgjson.version,
+    };
+
+    // 读取开发用签名公钥
+    let pair = await fs.readFile("./rootkeys/root.json", "utf-8");
+    pair = JSON.parse(pair);
+    const pairData = await stringToPair(pair);
+
+    // 签名data数据
+    console.log(pairData);
+
+    const verification = await sign(data, pairData);
+
     // 写入得到的数据
     await fs.writeFile(
       "./files.json",
       JSON.stringify({
-        data: {
-          files,
-          version: pgjson.version,
-        },
+        data,
+        verification,
       })
     );
 
-    console.log(files);
+    // console.log(files);
   } catch (err) {
     console.error(err);
   }
 })();
+
+/**
+ * 使用自身的密钥，给数据进行签名，并返回签名后的内容
+ * @param {String|Object} message 需要签名的数据
+ * @returns 签名字符串
+ */
+export async function sign(message, pair) {
+  let data = dataToArrayBuffer(message);
+
+  // 使用私钥进行签名
+  const signature = await crypto.subtle.sign(
+    {
+      name: "ECDSA",
+      hash: { name: "SHA-256" },
+    },
+    pair.privateKey,
+    data
+  );
+
+  return arrayBufferToBase64(signature);
+}
