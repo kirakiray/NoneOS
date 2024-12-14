c19a997e3e4891c1c91d4f96728fbb2f3ea5ba03
huang yao <5945154+kirakiray@users.noreply.github.com>
2024-12-14 18:13:00 +0800

    feat: support lang space

--- packages/i18n/data.js
+++ packages/i18n/data.js
@@ -2,3 +2,27 @@ export const data = $.stanz({
   lang: "cn",
   space: {},
 });
+
+export const spacePath = {};
+
+export const changeLang = async (lang) => {
+  data.lang = lang;
+  Object.entries(data.space).forEach(async ([key, langObj]) => {
+    const path = spacePath[key];
+    const langData = await fetch(`${path}/${lang}.json`).then((e) => e.json());
+
+    Object.assign(langObj, langData);
+  });
+};
+
+export const setSpace = async (name, path) => {
+  spacePath[name] = path;
+
+  const langData = await fetch(`${path}/${data.lang}.json`).then((e) =>
+    e.json()
+  );
+
+  if (!data.space[name]) {
+    data.space[name] = langData;
+  }
+};
