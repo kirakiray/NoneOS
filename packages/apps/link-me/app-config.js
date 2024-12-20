6c132b738ac565d4da8abc509df23b006e4b65e6
huang yao <5945154+kirakiray@users.noreply.github.com>
2024-12-20 15:49:29 +0800

    feat: add base link app

---
+++ packages/apps/link-me/app-config.js
@@ -0,0 +1,23 @@
+export const home = "./pages/home.html";
+
+export const pageAnime = {
+  current: {
+    opacity: 1,
+    transform: "translate(0, 0)",
+  },
+  next: {
+    opacity: 0,
+    transform: "translate(30px, 0)",
+  },
+  previous: {
+    opacity: 0,
+    transform: "translate(-30px, 0)",
+  },
+};
+
+export const onHandle = async function ({ handle, path }) {
+  // handle 和 path 必定会传入其中一个
+  console.log("onHandle: ", path, handle);
+};
+
+export const allowForward = true;
