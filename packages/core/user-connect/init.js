c1f3750febee6dc1f362a1552d9f3e5f351ec391
huang yao <5945154+kirakiray@users.noreply.github.com>
2025-01-09 18:18:55 +0800

    fix: some issuse

--- packages/core/user-connect/init.js
+++ packages/core/user-connect/init.js
@@ -1,14 +1,20 @@
 import { on, userMiddleware } from "../main.js";
 import { getId } from "../base/pair.js";
+import { getText, setSpace } from "/packages/i18n/data.js";
 
 {
   on("user-connected", async ({ data: { target } }) => {
+    await setSpace(
+      "files",
+      new URL("/packages/apps/files/lang", location.href).href
+    );
+
     // 连接成功时，发送一次可访问的目录
     target.send({
       type: "obtain-accessible-directories",
       dirs: [
         {
-          name: "虚拟空间",
+          name: getText("virLocal", "files"),
           path: `$remote:${await getId()}:local`,
         },
       ],
