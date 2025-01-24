244d8abf4d8abdb1dcbc8960c9092361fae268a1
huang yao <5945154+kirakiray@users.noreply.github.com>
2025-01-24 11:40:06 +0800

    feat: add export dir

--- packages/fs/task/main/export.js
+++ packages/fs/task/main/export.js
@@ -6,36 +6,42 @@ import { handleToZip } from "/packages/apps/files/util.js";
 export const runExportFileTask = async (options) => {
   const { from, delayTime } = options;
 
-  if (from.length === 1) {
-    const handle = await get(from[0]);
+  const targetTask = await addTaskData({
+    tips: ``,
+  });
 
-    const targetTask = await addTaskData({
-      tips: `正在下载 <b>${handle.name}</b>`,
-    });
+  try {
+    if (from.length === 1) {
+      const handle = await get(from[0]);
 
-    let finnalFile;
+      targetTask.tips = `正在下载 <b>${handle.name}</b>`;
 
-    if (handle.kind === "file") {
-      // 直接下载文件
-      finnalFile = await handle.file();
-    } else if (handle.kind === "dir") {
-      finnalFile = await handleToZip(handle);
+      let finnalFile;
+
+      if (handle.kind === "file") {
+        // 直接下载文件
+        finnalFile = await handle.file();
+      } else if (handle.kind === "dir") {
+        finnalFile = await handleToZip(handle);
+      }
+
+      const url = URL.createObjectURL(finnalFile);
+      const aEle = document.createElement("a");
+      aEle.href = url;
+      aEle.download = handle.name || finnalFile.name;
+      aEle.click();
+      setTimeout(() => {
+        URL.revokeObjectURL(url);
+      }, 1000);
+
+      targetTask.tips = `导出 <b>${handle.name}</b> 成功`;
+      targetTask.done = true;
+      return;
     }
 
-    const url = URL.createObjectURL(finnalFile);
-    const aEle = document.createElement("a");
-    aEle.href = url;
-    aEle.download = handle.name || finnalFile.name;
-    aEle.click();
-    setTimeout(() => {
-      URL.revokeObjectURL(url);
-    }, 1000);
-
-    targetTask.tips = `导出 <b>${handle.name}</b> 成功`;
-    targetTask.done = true;
-    return;
+    // TODO: 多文件导出
+    debugger;
+  } catch (err) {
+    targetTask.error = `导出 <b>${handle.name}</b> 出错`;
   }
-
-  // TODO: 多文件导出
-  debugger;
 };
