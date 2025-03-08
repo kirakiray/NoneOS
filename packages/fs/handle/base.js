a6513a97c84bad1ebbd7986ad39d6cdf3cdd88d5
huang yao <5945154+kirakiray@users.noreply.github.com>
2025-03-08 22:25:21 +0800

    feat: add resolveTargetAndName

--- packages/fs/handle/base.js
+++ packages/fs/handle/base.js
@@ -55,7 +55,48 @@ export class BaseHandle {
     });
   }
 
-  async moveTo() {}
+  async moveTo(targetHandle, name) {
+    const [finalTarget, finalName] = await resolveTargetAndName(
+      targetHandle,
+      name,
+      "move",
+      this
+    );
+  }
 
-  async copyTo() {}
+  async copyTo(targetHandle, name) {
+    const [finalTarget, finalName] = await resolveTargetAndName(
+      targetHandle,
+      name,
+      "copy",
+      this
+    );
+  }
 }
+
+const resolveTargetAndName = async (targetHandle, name, methodName, self) => {
+  // 处理第一个参数为字符串的情况
+  let finalTarget = targetHandle;
+  let finalName = name;
+
+  if (typeof targetHandle === "string") {
+    finalName = targetHandle;
+    finalTarget = await self.parent();
+  }
+  // 如果目标句柄和当前句柄相同，则不需要移动
+  if (await self.isSame(finalTarget)) {
+    return self;
+  }
+
+  // 检查目标路径是否为当前路径的子目录
+  const targetPath = finalTarget.path;
+  const currentPath = self.path;
+  if (targetPath.startsWith(currentPath + "/")) {
+    throw new Error(`Cannot ${methodName} a directory into its subdirectory`);
+  }
+
+  // 获取目标文件名，如果没有提供则使用原文件名
+  finalName = finalName || self.name;
+
+  return [finalTarget, finalName];
+};
