5e1f3a439048e0b34500901d5cc1e1bc2d800535
huang yao <5945154+kirakiray@users.noreply.github.com>
2025-03-08 21:06:26 +0800

    feat: add flat

--- packages/fs/handle/dir.js
+++ packages/fs/handle/dir.js
@@ -128,5 +128,19 @@ export class DirHandle extends BaseHandle {
   }
 
   // 扁平化获取所有的子文件（包括多级子孙代）
-  async flat() {}
+  async flat() {
+    const result = [];
+    // 遍历当前目录下的所有文件和文件夹
+    for await (const [name, handle] of this.entries()) {
+      // 只有非目录类型才加入结果数组
+      if (handle.kind !== "dir") {
+        result.push(handle);
+      } else {
+        // 如果是文件夹，只获取其子文件
+        const children = await handle.flat();
+        result.push(...children);
+      }
+    }
+    return result;
+  }
 }
