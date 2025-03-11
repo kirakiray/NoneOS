7fd9fad40524b4b07480dca44421ed38b737bed4
huang yao <5945154+kirakiray@users.noreply.github.com>
2025-03-11 20:30:44 +0800

    feat: add getApps

--- packages/none-os/app.js
+++ packages/none-os/app.js
@@ -1,4 +1,33 @@
+// 系统默认的apps
+const defaultAppsPaths = ["/packages/apps/bookmarks.napp"];
+
 // 获取所有app数组
 export const getApps = async () => {
-  return [];
+  const defaults = [];
+  await Promise.all(
+    defaultAppsPaths.map(async (path, index) => {
+      defaults[index] = await getFullAppData(path);
+    })
+  );
+
+  return [...defaults];
+};
+
+// 根据路径，获取完全的数据
+const getFullAppData = async (appDirPath) => {
+  const configUrl = `${appDirPath}/app.json`;
+  const configData = await fetch(configUrl).then((e) => e.json());
+
+  const iconUrlObj = new URL(
+    configData.icon,
+    new URL(configUrl, location.href)
+  );
+
+  return {
+    icon: {
+      url: iconUrlObj.href,
+    },
+    configUrl,
+    configData,
+  };
 };
