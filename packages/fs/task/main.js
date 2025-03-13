4e63738ca1c7111228e8b1223ecab92ba8141b95
huang yao <5945154+kirakiray@users.noreply.github.com>
2025-03-13 15:23:56 +0800

    feat: add task item anime

--- packages/fs/task/main.js
+++ packages/fs/task/main.js
@@ -9,7 +9,7 @@ export const addTask = async () => {};
 // 导入文件
 export const importFile = async (to) => {
   const taskItem = addTaskItem({
-    icon: "delete",
+    icon: "file",
     name: "等待选择文件",
   });
 
@@ -18,8 +18,12 @@ export const importFile = async (to) => {
     fileEl.type = "file";
     fileEl.multiple = true;
     fileEl.onchange = async (e) => {
-      debugger;
       resolve(e.target.files);
+    };
+    fileEl.oncancel = () => {
+      taskItem.name = "已取消导入文件";
+      taskItem.done = true;
+      reject();
     };
 
     fileEl.click();
@@ -27,3 +31,21 @@ export const importFile = async (to) => {
 
   debugger;
 };
+
+// setTimeout(() => {
+//   let taskItem = addTaskItem({
+//     icon: "file",
+//     name: "等待选择文件",
+//   });
+
+//   setTimeout(() => {
+//     taskItem = addTaskItem({
+//       icon: "file",
+//       name: "等待选择文件22",
+//     });
+//   }, 800);
+
+//   setTimeout(() => {
+//     taskItem.done = true;
+//   }, 1500);
+// }, 100);
