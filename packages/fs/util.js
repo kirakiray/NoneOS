0aed4afa500c9353c9895851d916509f3fffc618
huang yao <5945154+kirakiray@users.noreply.github.com>
2025-03-16 14:27:11 +0800

    Update util.js

--- packages/fs/util.js
+++ packages/fs/util.js
@@ -48,8 +48,13 @@ export const calculateFileChunkHashes = async (file) => {
   });
 };
 
-// 获取文件哈希值
-// 将文件切割成128kb的块，计算每个块的哈希值，然后将所有块的哈希值拼接起来，计算最终的哈希值
+/**
+ * 获取文件的总体哈希值
+ * @param {File} file - 要计算哈希的文件对象
+ * @returns {Promise<string>} 返回文件的总体哈希值
+ * @description 将文件分割成128kb大小的块，计算每个块的SHA-256哈希值，
+ *             然后将所有块的哈希值拼接成字符串，最后计算这个字符串的哈希值作为文件的总体哈希值
+ */
 export const getFileHash = async (file) => {
   const hashArray = await calculateFileChunkHashes(file);
   return getHash(hashArray.join(""));
