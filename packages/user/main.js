496313f022f6fa9470e049d4c34378710c05ce09
huang yao <5945154+kirakiray@users.noreply.github.com>
2025-03-17 19:51:10 +0800

    feat: add init data

---
+++ packages/user/main.js
@@ -0,0 +1,52 @@
+import { get } from "/packages/fs/main.js";
+import { createData } from "/packages/hybird-data/main.js";
+import { generateKeyPair } from "./cert.js";
+import { getHash } from "/packages/fs/util.js";
+
+const users = {
+  userDirName: "",
+};
+
+// 确保已经初始化了用户
+export const initUser = async (userDirName = "self") => {
+  if (users[userDirName]) {
+    return users[userDirName];
+  }
+
+  const userHandle = await get(`/system/${userDirName}`, {
+    create: "dir",
+  });
+
+  // 生成主体对象
+  const userStore = await createData(userHandle);
+
+  await userStore.ready();
+
+  let userid;
+
+  // 没有对钥，代表还未初始化
+  if (!userStore.pair) {
+    const pair = await generateKeyPair();
+    userStore.pair = pair;
+
+    await userStore.pair.ready();
+
+    // 计算用户id
+    userid = await getHash(pair.publicKey);
+
+    // 根据id生成初始用户名
+    userStore.userName = `user-${userid.slice(
+      userid.length / 2,
+      userid.length / 2 + 4
+    )}`;
+  }
+
+  if (!userid) {
+    await userStore.pair.ready();
+    userid = await getHash(userStore.pair.publicKey);
+  }
+
+  userStore._userid = userid;
+
+  return userStore;
+};
