b3107f8f74fbcbbdde9f0d94d35a65a49910ee26
huang yao <5945154+kirakiray@users.noreply.github.com>
2025-03-19 19:55:07 +0800

    Update post.js

--- server/handlers/post.js
+++ server/handlers/post.js
@@ -4,17 +4,38 @@ import { activeConnections, authenticatedUsers } from "../client.js";
 export const post = async ({ taskId, data }, client) => {
   const { type } = data;
 
+  let finnalData = {};
+
   if (type === "get-all") {
+    // 已经被认证成功的用户
+    const authenticateds = Array.from(authenticatedUsers).map(([userid, e]) => {
+      const { client } = e;
+      return {
+        sessionId: client.sessionId,
+        userInfo: client.userInfo,
+        userid: client._userId,
+      };
+    });
+
+    // 计算没有被认证成功的用户
+    const unauthenticateds = Array.from(activeConnections)
+      .filter((e) => !e._userId)
+      .map((client) => {
+        return {
+          sessionId: client.sessionId,
+        };
+      });
+
+    finnalData = {
+      unauthenticateds,
+      authenticateds,
+    };
   }
 
-  debugger;
   return {
     type: "post-response",
     taskId,
     success: 1,
-    data: {
-      activesLen: activeConnections.size,
-      authedLen: authenticatedUsers.size,
-    },
+    data: finnalData,
   };
 };
