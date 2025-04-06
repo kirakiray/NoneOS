c5fbdb231e25de2ef500c82e6a6d9ff53cbfc8cf
huang yao <5945154+kirakiray@users.noreply.github.com>
2025-04-06 13:50:35 +0800

    Update connection.js

--- packages/user/connection/connection.js
+++ packages/user/connection/connection.js
@@ -6,9 +6,6 @@ const iceServers = [
   { urls: "stun:stun.tutous.com" },
   { urls: "stun:stun.l.google.com:19302" },
   { urls: "stun:stun1.l.google.com:19302" },
-  { urls: "stun:stun2.l.google.com:19302" },
-  { urls: "stun:stun3.l.google.com:19302" },
-  { urls: "stun:stun4.l.google.com:19302" },
   { urls: "stun:stun.cloudflare.com" },
 ];
 
