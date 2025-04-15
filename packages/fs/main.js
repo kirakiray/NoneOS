0e76a26741c302c8b503c0acdf8c432d378b57af
huang yao <5945154+kirakiray@users.noreply.github.com>
2025-04-15 14:56:29 +0800

    Update main.js

--- packages/fs/main.js
+++ packages/fs/main.js
@@ -1,22 +1,24 @@
-import {
-  get as systemHandleGet,
-  init as systemHandleInit,
-} from "./handle/main.js";
+export { get, init } from "./handle/main.js";
 
-import { init as cHandleInit, get as cHandleGet } from "./c-handle/main.js";
+// import {
+//   get as systemHandleGet,
+//   init as systemHandleInit,
+// } from "./handle/main.js";
 
-// import { init as dbHandleInit, get as dbHandleGet } from "./db-handle/main.js";
+// import { init as cHandleInit, get as cHandleGet } from "./c-handle/main.js";
 
-import { isSafari } from "./util.js";
+// // import { init as dbHandleInit, get as dbHandleGet } from "./db-handle/main.js";
 
-export const init = async (name) => {
-  return !isSafari ? systemHandleInit(name) : cHandleInit(name);
-};
+// import { isSafari } from "./util.js";
 
-// safari 不支持 systemHandle，所以被迫重新实现一个虚拟系统
-export const get = async (path, options) => {
-  return !isSafari ? systemHandleGet(path, options) : cHandleGet(path, options);
-};
+// export const init = async (name) => {
+//   return !isSafari ? systemHandleInit(name) : cHandleInit(name);
+// };
+
+// // safari 不支持 systemHandle，所以被迫重新实现一个虚拟系统
+// export const get = async (path, options) => {
+//   return !isSafari ? systemHandleGet(path, options) : cHandleGet(path, options);
+// };
 
 // // 全量测试 c-handle
 // import { init as cHandleInit, get as cHandleGet } from "./c-handle/main.js";
