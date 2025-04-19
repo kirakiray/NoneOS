8bda4d0a48feadcacd4020a4a3c474074daf76d8
huang yao <5945154+kirakiray@users.noreply.github.com>
2025-04-19 21:31:01 +0800

    fix: file explore adress

--- packages/fs/remote/util/get-remotes.js
+++ packages/fs/remote/util/get-remotes.js
@@ -14,6 +14,7 @@ export const getRemotes = async ({ userDirName } = {}) => {
         {
           name: "local",
           path: `$user-${userId}:local`,
+          userId,
         },
       ],
     };
