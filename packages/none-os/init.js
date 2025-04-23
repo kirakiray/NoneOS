44010793b005f263b2c2ad0c79a15ada94db1c1c
huang yao <5945154+kirakiray@users.noreply.github.com>
2025-04-23 16:38:28 +0800

    Update init.js

--- packages/none-os/init.js
+++ packages/none-os/init.js
@@ -30,9 +30,52 @@ Object.defineProperties($.fn, {
       const mark = getAppMark(this[0]);
 
       const { getRemotes } = await load("/packages/util/get-remotes.js");
+      const { connect } = await load("/packages/user/connection/main.js");
+
+      const remotes = await getRemotes();
+
+      // 等待连接成功
+      const conns = await Promise.all(
+        remotes.map(async (remote) => {
+          const connection = connect({
+            userId: remote.userId,
+          });
+
+          await connection.watchUntil(
+            () =>
+              connection.state === "ready" ||
+              connection.state === "not-find-user"
+          );
+
+          return connection; // 返回连接对象或其他标识
+        })
+      );
+
+      const remoteHandles = [];
+
+      const { get } = await load("/packages/fs/main.js");
+      const { getUserName } = await load("/packages/util/get-user-info.js");
 
       // 从远端设备中获取handle
-    //   debugger;
+      await Promise.all(
+        conns.map(async (connection) => {
+          if (connection.state === "ready") {
+            const handle = await get(
+              `$user-${connection.userId}:local/dedicated/${mark}`
+            );
+
+            // 添加远端的handle
+            remoteHandles.push({
+              userName: await getUserName(connection.userId),
+              userId: connection.userId,
+              handle,
+              hasData: (await handle.length()) > 0, // 存在这个应用的数据
+            });
+          }
+        })
+      );
+
+      return remoteHandles;
     },
   },
 });
