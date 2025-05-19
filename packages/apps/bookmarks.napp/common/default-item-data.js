331b3285efe8a582d4b730f662d22f1c6b34b199
huang yao <5945154+kirakiray@users.noreply.github.com>
2025-05-19 20:34:54 +0800

    refactor(bookmarks): 提取默认书签数据到单独模块

---
+++ packages/apps/bookmarks.napp/common/default-item-data.js
@@ -0,0 +1,10 @@
+export const defaultItemData = {
+  url: "", // 网页地址
+  webTitle: "", // 网页标题
+  logoType: "default", // logo类型 default custom
+  defaultPic: "", // 默认logo文件地址，default类型下使用
+  customPic: "", // 自定义logo地址
+  selectedPic: "", // 选中的logo文件地址
+};
+
+export default defaultItemData;
