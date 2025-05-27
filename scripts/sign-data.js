4936397d6830fa7f9a1d5f86c921369518ca92dd
huang yao <5945154+kirakiray@users.noreply.github.com>
2025-05-27 13:40:06 +0800

    feat(installer): 改进安装页面下载流程和状态显示

---
+++ scripts/sign-data.js
@@ -0,0 +1,21 @@
+import { createSigner } from "../packages/user/util.js";
+
+export const signDataByPair = async (originData, pair) => {
+  let data = {
+    ...originData,
+    signTime: Date.now(),
+    publicKey: pair.publicKey,
+  };
+
+  // 生成签名器
+  const sign = await createSigner(pair.privateKey);
+
+  // 签名并转换为 base64
+  const signature = await sign(JSON.stringify(data));
+
+  return {
+    data,
+    // 将 ArrayBuffer 转换为 base64 字符串
+    signature: btoa(String.fromCharCode(...new Uint8Array(signature))),
+  };
+};
