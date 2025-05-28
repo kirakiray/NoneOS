9ef861bafe3e89b884ca67ad9fb79ed617d4795e
huang yao <5945154+kirakiray@users.noreply.github.com>
2025-05-28 22:59:57 +0800

    feat(sw): 添加本地包目录index.html处理逻辑

--- sw/main.js
+++ sw/main.js
@@ -1,6 +1,7 @@
 import resposeFs from "./response-fs.js";
 import responsePkg from "./response-pkg.js";
 import configs from "./config.js";
+import { getFile } from "./util.js";
 
 self.addEventListener("fetch", (event) => {
   const { request } = event;
@@ -11,10 +12,36 @@ self.addEventListener("fetch", (event) => {
     // 请求本地文件，会$开头
     if (/^\/\$/.test(pathname)) {
       resposeFs(event);
-    } else if (/^\/packages/.test(pathname)) {
+    } else if (!configs.packageUseOnline && /^\/packages/.test(pathname)) {
       // 访问包目录
       responsePkg(event);
     }
+  }
+
+  if (!configs.packageUseOnline && pathname === "/") {
+    // 尝试从packages中获取index.html
+    event.respondWith(
+      (async () => {
+        try {
+          // 获取文件
+          const fileHandle = await getFile("packages/index.html");
+
+          if (fileHandle) {
+            // 优先使用本地文件
+            return new Response(await fileHandle.getFile(), {
+              headers: {
+                "Content-Type": "text/html;charset=utf-8",
+              },
+            });
+          }
+        } catch (err) {
+          console.error(err);
+        }
+
+        // 直接返回index.html
+        return fetch(request);
+      })()
+    );
   }
 
   if (pathname === "/package-use-online") {
