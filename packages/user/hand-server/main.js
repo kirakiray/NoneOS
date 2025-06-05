d5c773ecfe105f110be419986ecaf3d4e5cc4ba0
huang yao <5945154+kirakiray@users.noreply.github.com>
2025-06-05 17:33:55 +0800

    Update main.js

--- packages/user/hand-server/main.js
+++ packages/user/hand-server/main.js
@@ -1,48 +1,57 @@
 import { getUserStore } from "../user-store.js";
 import { initServers } from "./init-servers.js";
 
+const pool = {};
+
 // 获取服务器列表
 export const getServers = async (useLocalUserDirName) => {
   useLocalUserDirName = useLocalUserDirName || "main";
-  const selfUserStore = await getUserStore(useLocalUserDirName);
-  await selfUserStore.ready(true);
 
-  // 初始化服务器列表
-  if (!selfUserStore.servers || selfUserStore.servers.length === 0) {
-    selfUserStore.servers = [];
-    if (location.host.includes("localhost")) {
-      await selfUserStore.servers.ready();
-
-      // 加入测试地址
-      selfUserStore.servers.push(
-        {
-          url: "ws://localhost:5579/",
-        },
-        {
-          url: "ws://localhost:5589/",
-        }
-      );
-
-      await selfUserStore.servers.ready(true);
-    } else {
-      // 加入正式地址
-      selfUserStore.servers.push(
-        {
-          url: "wss://hand-cn.tutous.com:812",
-        },
-        {
-          url: "wss://hand-ca.tutous.com",
-        }
-      );
-
-      await selfUserStore.servers.ready(true);
-    }
-
-    // 等待300ms，确保数据同步完成
-    await new Promise((resolve) => setTimeout(resolve, 300));
+  if (pool[useLocalUserDirName]) {
+    return pool[useLocalUserDirName];
   }
 
-  return await initServers(useLocalUserDirName);
+  return (pool[useLocalUserDirName] = new Promise(async (resolve) => {
+    const selfUserStore = await getUserStore(useLocalUserDirName);
+    await selfUserStore.ready(true);
+
+    // 初始化服务器列表
+    if (!selfUserStore.servers || selfUserStore.servers.length === 0) {
+      selfUserStore.servers = [];
+      if (location.host.includes("localhost")) {
+        await selfUserStore.servers.ready();
+
+        // 加入测试地址
+        selfUserStore.servers.push(
+          {
+            url: "ws://localhost:5579/",
+          },
+          {
+            url: "ws://localhost:5589/",
+          }
+        );
+
+        await selfUserStore.servers.ready(true);
+      } else {
+        // 加入正式地址
+        selfUserStore.servers.push(
+          {
+            url: "wss://hand-cn.tutous.com:812",
+          },
+          {
+            url: "wss://hand-ca.tutous.com",
+          }
+        );
+
+        await selfUserStore.servers.ready(true);
+      }
+
+      // 等待300ms，确保数据同步完成
+      await new Promise((resolve) => setTimeout(resolve, 300));
+    }
+
+    resolve(await initServers(useLocalUserDirName));
+  }));
 };
 
 // 添加服务器
