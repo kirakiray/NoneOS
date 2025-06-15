9a53c92ef2e97b12ef509086e4315fcdcbff86b7
huang yao <5945154+kirakiray@users.noreply.github.com>
2025-06-15 12:06:18 +0800

    refactor: 改个更好的名字

--- packages/none-os/app.js
+++ packages/none-os/app.js
@@ -5,7 +5,7 @@ const defaultAppsPaths = [
   "/packages/apps/setting.napp",
   "/packages/apps/file-manager.napp",
   "/packages/apps/link-me.napp",
-  "/packages/apps/explain-note.napp",
+  "/packages/apps/luminote.napp",
   "/packages/apps/bookmarks.napp",
   "/packages/apps/text-edit.napp",
   "/packages/apps/picture.napp",
