d308572864d2966064a157d7480864f2ced881f0
huang yao <5945154+kirakiray@users.noreply.github.com>
2025-06-18 16:01:16 +0800

    feat(project): 实现项目管理和UI改进

--- packages/apps/luminote.napp/app-config.js
+++ packages/apps/luminote.napp/app-config.js
@@ -19,56 +19,137 @@ export const allowForward = true;
 
 import { createData } from "/packages/hybird-data/main.js";
 
+const exitedProject = {};
+
 export default {
+  proto: {
+    // 获取已经存在的项目名
+    async getExistProjects() {},
+
+    // 获取项目
+    getProject(dirName) {
+      if (!dirName) {
+        return new Error(`不存在该项目: ${dirName}`);
+      }
+
+      if (exitedProject[dirName]) {
+        return exitedProject[dirName];
+      }
+
+      return (exitedProject[dirName] = (async () => {
+        // 获取专属文件句柄
+        const rootHandle = await this.dedicatedHandle();
+
+        // 获取目标项目目录handle
+        const projectHandle = await rootHandle.get(dirName, {
+          create: "dir",
+        });
+
+        // 获取文章专属目录
+        const articleHandle = await projectHandle.get("article", {
+          create: "dir",
+        });
+
+        // 生成 article 数据
+        const articleData = await createData(articleHandle, {
+          saveDebounce: 500, // 数据变动后，500毫秒保存一次
+        });
+
+        await articleData.ready(true); // 准备完成
+
+        if (!articleData.main) {
+          articleData.projectName = "项目-" + dirName;
+          articleData.creationtime = Date.now();
+
+          // main 为主体的文章数据
+          articleData.main = [
+            {
+              title: "示范页面",
+              creationtime: Date.now(),
+              content: [
+                {
+                  type: "paragraph",
+                  value: "这是一个示范页面。",
+                },
+                {
+                  type: "paragraph",
+                  value: "",
+                },
+              ],
+            },
+          ];
+
+          await articleData.ready(true);
+        }
+
+        return {
+          data: articleData,
+          handle: projectHandle,
+        };
+      })());
+    },
+
+    // 删除项目
+    async deleteProject(dirName) {
+      debugger;
+    },
+
+    // 创建一个项目
+    async createProject() {
+      // 获取专属文件句柄
+      const handle = await this.dedicatedHandle();
+    },
+    // 释放所有已加载项目的内存资源
+    async revokeAllProject() {
+      debugger;
+
+      Object.values(exitedProject).forEach((e) => {
+        e.data.disconnect();
+      });
+    },
+  },
   ready() {
     this._articleData = (async () => {
       // await new Promise((res) => setTimeout(res, 100));
 
+      const project = await this.getProject("start");
+
+      return project.data;
+
       // 获取专属文件句柄
-      const handle = await this.dedicatedHandle();
+      // const handle = await this.dedicatedHandle();
 
-      // 获取文章专属目录
-      const articleHandle = await handle.get("article", {
-        create: "dir",
-      });
+      // // 获取文章专属目录
+      // const articleHandle = await handle.get("article", {
+      //   create: "dir",
+      // });
 
-      const articleData = await createData(articleHandle, {
-        saveDebounce: 500,
-      });
-      await articleData.ready(true); // 准备完成
+      // const articleData = await createData(articleHandle, {
+      //   saveDebounce: 500,
+      // });
 
-      if (!articleData.main) {
-        // 尝试测试用的文章
-        articleData.main = [
-          {
-            title: "示范页面",
-            content: [
-              {
-                type: "h1",
-                value: "基本的文本编辑功能：加粗、斜体、下划线和插入图片",
-              },
-              {
-                type: "paragraph",
-                value:
-                  "您可以通过继承 TextEditor 类来扩展这个编辑器的功能，或者直接使用它作为一个简单的富文本编辑器。",
-              },
-              {
-                type: "paragraph",
-                value:
-                  "如果您需要添加更多功能，比如字体大小、颜色选择、列表等，可以在工具栏中添加相应的按钮，并在 handleToolbarAction 方法中实现相应的功能。",
-              },
-              {
-                type: "paragraph",
-                value: "",
-              },
-            ],
-          },
-        ];
-      }
+      // await articleData.ready(true); // 准备完成
 
-      console.log("articleData:", articleData);
+      // if (!articleData.main) {
+      //   // 尝试测试用的文章
+      //   articleData.main = [
+      //     {
+      //       title: "示范页面",
+      //       content: [
+      //         {
+      //           type: "paragraph",
+      //           value: "这是一个示范页面。",
+      //         },
+      //         {
+      //           type: "paragraph",
+      //           value: "",
+      //         },
+      //       ],
+      //     },
+      //   ];
+      // }
 
-      return articleData;
+      // return articleData;
     })();
   },
 };
