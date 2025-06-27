303ccbfb2fd7def20320ea9377daf27b96d6e347
huang yao <5945154+kirakiray@users.noreply.github.com>
2025-06-27 10:19:26 +0800

    feat(面包屑导航): 添加面包屑导航功能并优化文章查找逻辑

--- packages/apps/luminote.napp/util/find-article-data.js
+++ packages/apps/luminote.napp/util/find-article-data.js
@@ -8,14 +8,36 @@ export const findArticle = async (app, dataId) => {
 const findTargetArticle = async (content, dataId) => {
   for (let e of content) {
     if (e._dataId === dataId) {
-      return { target: e };
+      return {
+        target: e,
+        titles: [
+          {
+            title: e.title,
+            dataId: e._dataId,
+          },
+        ],
+      };
     }
 
     if (e.content) {
-      let result = await findTargetArticle(e.content, dataId);
-      if (result?.target) {
-        return { target: result.target };
+      let { target, titles: subTitles } = await findTargetArticle(
+        e.content,
+        dataId
+      );
+      if (target) {
+        return {
+          target,
+          titles: [
+            {
+              title: e.title,
+              dataId: e._dataId,
+            },
+            ...subTitles,
+          ],
+        };
       }
     }
   }
+
+  return {};
 };
