b99ea4c9addf2f7792fe272ed3a987f5ce5b6e97
huang yao <5945154+kirakiray@users.noreply.github.com>
2025-07-03 23:23:43 +0800

    fix(ui): 移除冗余的load()调用并修复工具提示事件

--- packages/apps/luminote.napp/lumi/inline/config.js
+++ packages/apps/luminote.napp/lumi/inline/config.js
@@ -6,7 +6,7 @@ export const inlineComps = [
       "/packages/apps/luminote.napp/lumi/inline/lumi-quote/lumi-quote-form.html",
     tag: "lumi-quote",
     formTag: "lumi-quote-form",
-    icon: '<svg xmlns="http://www.w3.org/2000/svg" width="16px" height="16px" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M7 8.25a2.75 2.75 0 1 0 0-5.5a2.75 2.75 0 0 0 0 5.5m0 0v7.5m0-7.5c0 2.9 2.35 5.25 5.25 5.25h2M7 15.75a2.75 2.75 0 1 0 0 5.5a2.75 2.75 0 0 0 0-5.5m7.25-2.25a2.75 2.75 0 1 0 5.5 0a2.75 2.75 0 0 0-5.5 0"/></svg>',
+    icon: '<svg xmlns="http://www.w3.org/2000/svg" width="16px" height="16px" viewBox="0 0 48 48"><defs><mask id="ipSDocAdd0"><g fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="4"><path fill="#fff" stroke="#fff" d="M38 4H10a2 2 0 0 0-2 2v36a2 2 0 0 0 2 2h28a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2"/><path stroke="#000" d="M17 30h14m-14 6h7m-5-19h10m-5 5V12"/></g></mask></defs><path fill="currentColor" d="M0 0h48v48H0z" mask="url(#ipSDocAdd0)"/></svg>',
     name: {
       en: "Create Explanation Note",
       cn: "解释这段文字",
@@ -14,6 +14,10 @@ export const inlineComps = [
     desc: {
       en: "Create a new note to explain the selected content.",
       cn: "新建一份笔记，对选中内容进行解释说明。",
+    },
+    // 点击内联组件时触发
+    click() {
+      debugger;
     },
   },
 ];
