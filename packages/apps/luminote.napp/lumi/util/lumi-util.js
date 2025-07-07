af1cba311ebf0466b2f67616850e35333f7ebc14
huang yao <5945154+kirakiray@users.noreply.github.com>
2025-07-07 15:16:39 +0800

    refactor(lumi): 重构工具模块并拆分功能到不同文件

---
+++ packages/apps/luminote.napp/lumi/util/lumi-util.js
@@ -0,0 +1,34 @@
+// 队列中的block元素
+const blocks = [];
+let running = false;
+export const addLoadingTask = (lumiBlock) => {
+  blocks.push(lumiBlock);
+
+  if (!running) {
+    runBlock();
+  }
+};
+
+export const removeLoadingTask = (lumiBlock) => {
+  const index = blocks.indexOf(lumiBlock);
+  if (index !== -1) {
+    blocks.splice(index, 1);
+  }
+};
+
+// 按顺序渲染组件，防止页面卡顿
+export const runBlock = async () => {
+  running = true;
+
+  const current = blocks.shift();
+
+  current.loading = false;
+
+  await new Promise((res) => setTimeout(res, 0)); // 异步渲染组件
+
+  if (blocks.length) {
+    runBlock();
+  } else {
+    running = false;
+  }
+};
