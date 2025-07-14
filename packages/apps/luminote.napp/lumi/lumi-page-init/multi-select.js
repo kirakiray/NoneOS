0996c05336d40da808852f42353efc845172af06
huang yao <5945154+kirakiray@users.noreply.github.com>
2025-07-14 22:51:22 +0800

    fix(lumi-page-init): 修复多选功能的事件监听未正确移除的问题

--- packages/apps/luminote.napp/lumi/lumi-page-init/multi-select.js
+++ packages/apps/luminote.napp/lumi/lumi-page-init/multi-select.js
@@ -1,109 +1,118 @@
 let mousedownStartBlock = null;
 
 export const initMultiSelect = (lumipage) => {
-  lumipage.on("mousedown", (e) => {
-    const [lumiBlock] = e
-      .composedPath()
-      .filter((e) => e.tagName === "LUMI-BLOCK");
+  lumipage.on(
+    "mousedown",
+    (lumipage._multiSelectMousedownFunc = (e) => {
+      const [lumiBlock] = e
+        .composedPath()
+        .filter((e) => e.tagName === "LUMI-BLOCK");
 
-    if (!lumiBlock) {
-      return;
-    }
-
-    lumipage.forEach((e) => {
-      e.selected = null;
-    });
-
-    mousedownStartBlock = lumiBlock; // 记录鼠标按下的block
-  });
-
-  lumipage.on("mouseover", (e) => {
-    const [lumiBlock] = e
-      .composedPath()
-      .filter((e) => e.tagName === "LUMI-BLOCK");
-
-    if (!lumiBlock || !mousedownStartBlock) {
-      return;
-    }
-
-    const { startIndex, endIndex } = getStartAndEnd(lumiBlock, lumipage);
-
-    // 打上选中的标记
-    lumipage.forEach((e, i) => {
-      if (i >= startIndex && i <= endIndex) {
-        e.selected = true;
-      } else {
-        e.selected = null;
+      if (!lumiBlock) {
+        return;
       }
-    });
 
-    if (startIndex === endIndex) {
-      $(lumiBlock).selected = null;
-    }
-  });
-
-  lumipage.on("mouseup", (e) => {
-    const [lumiBlock] = e
-      .composedPath()
-      .filter((e) => e.tagName === "LUMI-BLOCK");
-
-    if (!lumiBlock || !mousedownStartBlock) {
-      return;
-    }
-
-    const { startIndex, endIndex } = getStartAndEnd(lumiBlock, lumipage);
-
-    if (startIndex === endIndex) {
-      mousedownStartBlock = null;
-      return;
-    }
-
-    // 选中的block
-    const selectedBlocks = lumipage.slice(startIndex, endIndex + 1);
-
-    const multiPanel = lumipage.shadow.$("lumi-multi-panel");
-
-    if (!multiPanel) {
-      console.error("lumi-multi-panel not found");
-      return;
-    }
-
-    multiPanel._selecteds = selectedBlocks;
-    multiPanel.open = true; // 打开
-
-    const originEvent = e;
-
-    multiPanel.multiPanelStyle = {
-      left: `${originEvent.clientX}px`,
-      top: `${originEvent.clientY}px`,
-    };
-
-    // // 如果超出了屏幕边, 改为靠右
-    const width = parseInt(multiPanel.panel.css.width);
-
-    console.log("panelwidth: ", width);
-
-    const selfRect = lumipage.ele.getBoundingClientRect();
-
-    if (width + originEvent.clientX > selfRect.width + selfRect.left) {
-      multiPanel.multiPanelStyle.left =
-        selfRect.width - width + selfRect.left + "px";
-    }
-
-    multiPanel.one("close", () => {
       lumipage.forEach((e) => {
         e.selected = null;
-        multiPanel._selecteds = [];
       });
-    });
 
-    // 取消原来选中的范围
-    window.getSelection().removeAllRanges();
+      mousedownStartBlock = lumiBlock; // 记录鼠标按下的block
+    })
+  );
 
-    console.log("selectedBlocks: ", selectedBlocks);
+  lumipage.on(
+    "mouseover",
+    (lumipage._multiSelectMouseoverFunc = (e) => {
+      const [lumiBlock] = e
+        .composedPath()
+        .filter((e) => e.tagName === "LUMI-BLOCK");
 
-    mousedownStartBlock = null;
-  });
+      if (!lumiBlock || !mousedownStartBlock) {
+        return;
+      }
+
+      const { startIndex, endIndex } = getStartAndEnd(lumiBlock, lumipage);
+
+      // 打上选中的标记
+      lumipage.forEach((e, i) => {
+        if (i >= startIndex && i <= endIndex) {
+          e.selected = true;
+        } else {
+          e.selected = null;
+        }
+      });
+
+      if (startIndex === endIndex) {
+        $(lumiBlock).selected = null;
+      }
+    })
+  );
+
+  lumipage.on(
+    "mouseup",
+    (lumipage._multiSelectMouseupFunc = (e) => {
+      const [lumiBlock] = e
+        .composedPath()
+        .filter((e) => e.tagName === "LUMI-BLOCK");
+
+      if (!lumiBlock || !mousedownStartBlock) {
+        return;
+      }
+
+      const { startIndex, endIndex } = getStartAndEnd(lumiBlock, lumipage);
+
+      if (startIndex === endIndex) {
+        mousedownStartBlock = null;
+        return;
+      }
+
+      // 选中的block
+      const selectedBlocks = lumipage.slice(startIndex, endIndex + 1);
+
+      const multiPanel = lumipage.shadow.$("lumi-multi-panel");
+
+      if (!multiPanel) {
+        console.error("lumi-multi-panel not found");
+        return;
+      }
+
+      multiPanel._selecteds = selectedBlocks;
+      multiPanel.open = true; // 打开
+
+      const originEvent = e;
+
+      multiPanel.multiPanelStyle = {
+        left: `${originEvent.clientX}px`,
+        top: `${originEvent.clientY}px`,
+      };
+
+      // // 如果超出了屏幕边, 改为靠右
+      const width = parseInt(multiPanel.panel.css.width);
+
+      console.log("panelwidth: ", width);
+
+      const selfRect = lumipage.ele.getBoundingClientRect();
+
+      if (width + originEvent.clientX > selfRect.width + selfRect.left) {
+        multiPanel.multiPanelStyle.left =
+          selfRect.width - width + selfRect.left + "px";
+      }
+
+      multiPanel.one("close", () => {
+        lumipage.forEach((e) => {
+          e.selected = null;
+          multiPanel._selecteds = [];
+        });
+      });
+
+      // 取消原来选中的范围
+      window.getSelection().removeAllRanges();
+
+      console.log("selectedBlocks: ", selectedBlocks);
+
+      mousedownStartBlock = null;
+    })
+  );
 };
 
 const getStartAndEnd = (lumiBlock, lumipage) => {
@@ -118,5 +127,7 @@ const getStartAndEnd = (lumiBlock, lumipage) => {
 };
 
 export const revokeMultiSelect = (lumipage) => {
-  debugger;
+  lumipage.off("mousedown", lumipage._multiSelectMousedownFunc);
+  lumipage.off("mouseover", lumipage._multiSelectMouseoverFunc);
+  lumipage.off("mouseup", lumipage._multiSelectMouseupFunc);
 };
