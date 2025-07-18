d5198c2c408fec182737e6f815d7bd0016822ffe
huang yao <5945154+kirakiray@users.noreply.github.com>
2025-07-18 13:07:36 +0800

    feat(block): 添加复选框组件支持

--- packages/apps/luminote.napp/lumi/block/config.js
+++ packages/apps/luminote.napp/lumi/block/config.js
@@ -1,3 +1,4 @@
 import code from "./lumi-code/config.js";
+import checkbox from "./lumi-checkbox/config.js";
 
-export const blockComps = [code];
+export const blockComps = [code, checkbox];
---
+++ packages/apps/luminote.napp/lumi/block/lumi-checkbox/config.js
@@ -0,0 +1,18 @@
+const defaults = {
+  tag: "lumi-checkbox",
+  src: "/packages/apps/luminote.napp/lumi/block/lumi-checkbox/lumi-checkbox.html",
+  icon: `<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="M2 2h13v5.5h-2V4H4v9h3.5v2H2zm7 7h13v13H9zm2 2v9h9v-9zm8.414 3L15 18.414L12.086 15.5l1.414-1.414l1.5 1.5l3-3z"/></svg>`,
+  name: {
+    en: "Checkbox",
+    cn: "复选框",
+  },
+  desc: {
+    en: "A component for checkbox display",
+    cn: "复选框的组件",
+  },
+  _getUseContenteditable() {
+    return true;
+  },
+};
+
+export default defaults;
