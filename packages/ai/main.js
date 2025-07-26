d37f6ca971d1544a5596df408c5a266615b01798
huang yao <5945154+kirakiray@users.noreply.github.com>
2025-07-26 23:11:17 +0800

    feat(ai): 改进本地AI模型交互逻辑

--- packages/ai/main.js
+++ packages/ai/main.js
@@ -7,18 +7,49 @@ export {
   askOllamaStream,
 } from "./ollama.js";
 
-export const ask = async (userPrompt, { model: modelName, callback }) => {
+import { askOllamaStream } from "./ollama.js";
+
+export const modelExtend = {
+  "qwen3:4b": {
+    prepare(prompt) {
+      return `/no_think${prompt}`;
+    },
+    preprocess(responseText) {
+      if (!/<think>[\d\D]*?<\/think>\n/.test(responseText)) {
+        return "";
+      }
+      
+      return responseText.replace(/^\<think>[\d\D]*?<\/think>\s+/, "");
+    },
+  },
+};
+
+export const ask = async (prompt, { model: modelName, onChunk }) => {
+  const settingData = await getSetting();
+
   let responseText = "";
 
-  let finalModelName = modelName || "qwen3:4b";
+  let finalModelName = modelName || settingData.ai.model || "qwen3:4b";
 
-  await askOllamaStream(userPrompt, finalModelName, (chunk) => {
+  const targetExtend = modelExtend[finalModelName];
+
+  if (targetExtend) {
+    prompt = targetExtend.prepare(prompt);
+  }
+
+  await askOllamaStream(prompt, finalModelName, (chunk) => {
     responseText += chunk;
 
-    if (callback) {
-      callback({
+    let preprocessedText = "";
+    if (targetExtend && targetExtend.preprocess) {
+      preprocessedText = targetExtend.preprocess(responseText);
+    }
+
+    if (onChunk) {
+      onChunk({
         modelName: finalModelName,
-        responseText,
+        originText: responseText,
+        responseText: preprocessedText,
         currentToken: chunk,
       });
     }
