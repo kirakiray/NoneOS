ad06c6906bfb9fa5aaaea536fd29915bc2f5ac54
huang yao <5945154+kirakiray@users.noreply.github.com>
2025-08-05 18:25:24 +0800

    feat(内联组件): 添加创建解释笔记功能并更新删除逻辑

--- packages/apps/luminote.napp/lumi/inline/config.js
+++ packages/apps/luminote.napp/lumi/inline/config.js
@@ -1,4 +1,5 @@
-import quote from "./lumi-quote/config.js";
+// import quote from "./lumi-quote/config.js";
+import createArticle from "./lumi-create-article/config.js";
 import link from "./lumi-link/config.js";
 
-export const inlineComps = [link, quote];
+export const inlineComps = [link, createArticle];
