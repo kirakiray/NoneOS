3316ef31269c278102d94d75730824d290896df7
huang yao <5945154+kirakiray@users.noreply.github.com>
2025-08-09 11:55:39 +0800

    feat(项目设置): 添加设置默认语言功能

---
+++ packages/apps/luminote.napp/lumi/util/change-lang.js
@@ -0,0 +1,49 @@
+// 改变项目的主体语言
+import { getHash } from "/packages/fs/util.js";
+export const changeLang = async (projectData, lang) => {
+  for (let articleData of projectData.main) {
+    await changeArticle(articleData, lang, projectData.mainLang);
+  }
+
+  // 最后更改主体语言
+  projectData.mainLang = lang;
+};
+
+const changeArticle = async (articleData, lang, originLang) => {
+  for (let item of articleData.content) {
+    const { i18nContent, value } = item;
+
+    const reValue = i18nContent[lang]?.value || value;
+
+    const hash = await getHash(reValue);
+
+    // 将主体替换为要切换的语言的内容
+    item.value = reValue;
+
+    // 删除旧的语言对象
+    if (i18nContent[lang]) {
+      delete i18nContent[lang];
+    }
+
+    // 保存翻译前的内容
+    i18nContent[originLang] = {
+      modelName: "",
+      originHash: hash,
+      time: Date.now(),
+      value: value,
+    };
+
+    // 修正剩余的对象的hash
+    for (let [key, obj] of Object.entries(i18nContent)) {
+      if (key === "dataStatus" || key === originLang) {
+        continue;
+      }
+
+      obj.originHash = hash;
+    }
+
+    if (item.type === "article") {
+      await changeArticle(item, lang, originLang);
+    }
+  }
+};
