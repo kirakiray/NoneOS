b62f6ab5ac8b2c86eca3ac6265405ff29eb741cb
huang yao <5945154+kirakiray@users.noreply.github.com>
2025-08-09 12:44:35 +0800

    fix: 移除繁体中文选项并优化语言切换逻辑

--- packages/apps/luminote.napp/lumi/util/change-lang.js
+++ packages/apps/luminote.napp/lumi/util/change-lang.js
@@ -1,12 +1,23 @@
 // 改变项目的主体语言
 import { getHash } from "/packages/fs/util.js";
+
 export const changeLang = async (projectData, lang) => {
   for (let articleData of projectData.main) {
     await changeArticle(articleData, lang, projectData.mainLang);
   }
 
+  const oldMainLang = projectData.mainLang;
+
   // 最后更改主体语言
   projectData.mainLang = lang;
+
+  const langIndex = projectData.otherLang.indexOf(lang);
+  if (langIndex !== -1) {
+    projectData.otherLang.splice(langIndex, 1);
+  }
+
+  // 最后添加到 otherLang 数组
+  projectData.otherLang.unshift(oldMainLang);
 };
 
 const changeArticle = async (articleData, lang, originLang) => {
