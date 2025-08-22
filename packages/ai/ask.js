67931aaaa23d4a843ca509a58c0801b96e79a55b
Yew <kirayao@hotmail.com>
2025-08-22 13:15:06 +0800

    Fix: Added AI not being used (#111)
    * feat(ai): 添加AI可用性检查及错误状态显示
    
    * refactor(ai): 重构 AI 配置获取逻辑以支持多配置
    
    * Update ask.js
    
    * chore: bump version
    
    ---------
    
    Co-authored-by: huang yao <5945154+kirakiray@users.noreply.github.com>

--- packages/ai/ask.js
+++ packages/ai/ask.js
@@ -1,15 +1,37 @@
 import { chat } from "./lmstudio.js";
-import { getAISetting } from "./custom-data.js";
+import { getAvailableAIConfigs } from "./main.js";
 
+let availableConfigs = null;
+
+/**
+ * 向 AI 模型发送请求并处理响应
+ * @param {string} prompt - 用户输入的提示
+ * @param {Object} options - 配置选项
+ * @param {Function} options.onChunk - 处理每个响应块的回调函数
+ * @returns {Promise<Object>} - 包含模型名称和完整响应的对象
+ */
 export const ask = async (prompt, options) => {
-  const aiSetting = await getAISetting();
+  if (!availableConfigs) {
+    availableConfigs = await getAvailableAIConfigs();
+
+    setTimeout(() => {
+      availableConfigs = null;
+    }, 1000 * 60);
+  }
+
+  if (!availableConfigs.length) {
+    availableConfigs = null;
+    throw new Error("No available AI configs");
+  }
 
   const { onChunk } = options;
 
-  const model = aiSetting.lmstudio.model;
+  const configData = availableConfigs[0];
+
+  const model = configData.model;
 
   const result = await chat({
-    serverUrl: aiSetting.lmstudio.serverUrl,
+    serverUrl: configData.url,
     model,
     messages: [{ role: "user", content: prompt }],
     onChunk: (e) => {
