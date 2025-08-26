a7670a5d1699e22000bba37e059c71c2a701599b
huang yao <5945154+kirakiray@users.noreply.github.com>
2025-08-26 20:01:15 +0800

    backup

---
+++ others/doc-static/_static/app.js
@@ -0,0 +1,14 @@
+export const pageAnime = {
+  current: {
+    opacity: 1,
+    transform: "translate(0, 0)",
+  },
+  next: {
+    opacity: 0,
+    transform: "translate(0, 30px)",
+  },
+  previous: {
+    opacity: 0,
+    transform: "translate(0, 30px)",
+  },
+};
