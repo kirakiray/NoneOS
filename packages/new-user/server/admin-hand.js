b668bc1a88440b57907e9ed43f6ed4be89341dd6
huang yao <5945154+kirakiray@users.noreply.github.com>
2025-10-04 23:12:29 +0800

    refactor(admin-hand): 将refreshClientsList重命名为getClients并返回Promise

--- packages/new-user/server/admin-hand.js
+++ packages/new-user/server/admin-hand.js
@@ -7,14 +7,30 @@ export class AdminHandServerClient extends HandServerClient {
   }
 
   // 获取在线列表用户数据
-  async refreshClientsList() {
+  // async refreshClientsList() {
+  async getClients() {
     if (this.socket && this.socket.readyState === WebSocket.OPEN) {
+      // 等待服务器响应
       this.socket.send(
         JSON.stringify({
           type: "get_connections",
           password: this.password,
         })
       );
+
+      return new Promise((resolve, reject) => {
+        const listener = (event) => {
+          const { type, clients } = event.detail;
+          if (type === "connections_info") {
+            this.removeEventListener("message", listener);
+            resolve(clients);
+          } else {
+            this.removeEventListener("message", listener);
+            reject(new Error("服务器返回错误类型"));
+          }
+        };
+        this.addEventListener("message", listener);
+      });
     } else {
       throw new Error("WebSocket连接未建立，无法发送刷新请求");
     }
