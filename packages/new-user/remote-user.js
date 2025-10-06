091d2f0020737927b7c2ea599365dfeebfdd37a4
huang yao <5945154+kirakiray@users.noreply.github.com>
2025-10-06 23:09:47 +0800

    Update remote-user.js

--- packages/new-user/remote-user.js
+++ packages/new-user/remote-user.js
@@ -76,24 +76,15 @@ export class RemoteUser extends BaseUser {
       throw new Error("未连接到对方");
     }
 
+    const msgId = Math.random().toString(32).slice(2);
+
     // 直接通过第一个发送
     this.#servers[0].sendTo(
       {
         userId: this.userId,
-        msgId: Math.random().toString(32).slice(2),
+        msgId,
       },
       msg
     );
-
-    // // 发送消息给服务端
-    // for (let server of this.#servers) {
-    //   server.sendTo(
-    //     {
-    //       userId: this.userId,
-    //       msgId: Math.random().toString(32).slice(2),
-    //     },
-    //     msg
-    //   );
-    // }
   }
 }
