b815801d206f2df17c93d7e1eddb9c52ce7a05c4
huang yao <5945154+kirakiray@users.noreply.github.com>
2025-10-17 14:22:25 +0800

    Update card-manager.js

--- packages/new-user/card-manager.js
+++ packages/new-user/card-manager.js
@@ -14,7 +14,19 @@ export class CardManager {
     debugger;
   }
 
-  async get({ userId, signature }) {
+  async get(options) {
+    let userId, signature; // 如果有userId，就根据userId查询；如果有signature，就根据signature查询；userId优先；
+
+    if (typeof options === "string") {
+      userId = options;
+    } else if (options.userId || options.signature) {
+      ({ userId, signature } = options);
+    }
+
+    debugger;
+  }
+
+  async delete() {
     debugger;
   }
 }
