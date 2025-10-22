a293fcf7417280cd82468f740b1c9ec97c932fa0
huang yao <5945154+kirakiray@users.noreply.github.com>
2025-10-22 15:59:08 +0800

    feat(i18n): 添加同步获取本地化文本的方法

--- packages/i18n/localized-object.js
+++ packages/i18n/localized-object.js
@@ -1,8 +1,12 @@
 import { getSetting } from "../none-os/setting.js";
 
+let currentLang = "en";
+
 export const getLocalized = async (obj) => {
   const settingData = await getSetting();
   const { lang } = settingData;
+
+  currentLang = settingData.lang;
 
   if (!obj[lang]) {
     return obj.en || obj.cn;
@@ -11,12 +15,22 @@ export const getLocalized = async (obj) => {
   return obj[lang];
 };
 
+export const getLocalizedSync = (obj) => {
+  if (!obj[currentLang]) {
+    return obj.en || obj.cn;
+  }
+
+  return obj[currentLang];
+};
+
 export const localizedObject = (obj) => {
   const reobj = $.stanz({});
 
   getSetting().then((settingData) => {
     const refresh = () => {
       const { lang } = settingData;
+
+      currentLang = lang;
 
       Object.keys(obj).forEach((key) => {
         reobj[key] = obj[key][lang];
