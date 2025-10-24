95bd1722020b39eef7ba287514789c640e58fb21
huang yao <5945154+kirakiray@users.noreply.github.com>
2025-10-24 14:37:40 +0800

    ci(test): 更新CI测试脚本配置

--- scripts/ci-test.js
+++ scripts/ci-test.js
@@ -1,4 +1,4 @@
-import { initServer as initSer1 } from "../server/main.js";
+import { initServer as initSer1 } from "../server/src/index.js";
 import { initServer as initSer2 } from "../server/dist.js";
 import staticSer from "./static-server.js";
 import shell from "shelljs";
@@ -6,24 +6,22 @@ import shell from "shelljs";
 // staticSer.server.close();
 
 const wss1 = initSer1({
-  port: 5579,
-  name: "local-test-handserver",
-  admin: ["1d14d3b31a76eeab220b48f562521f9b023e362119149072dcc02add230351b0"],
+  port: 18290,
+  serverName: "local-test-handserver",
 });
 
 const wss2 = initSer2({
-  port: 5589,
-  name: "local-test-handserver2",
-  admin: ["1d14d3b31a76eeab220b48f562521f9b023e362119149072dcc02add230351b0"],
+  port: 18291,
+  serverName: "local-test-handserver2",
 });
 
 shell.exec(`npx playwright test`, function (code, stdout, stderr) {
   console.log("Exit code:", code);
   console.log("Program output:", stdout);
   console.log("Program stderr:", stderr);
-  staticSer.server.close();
   // 关闭整个进程
   process.exit();
+  // staticSer.server.close();
   //   wss1.close();
   //   wss2.close();
   if (code !== 0) {
