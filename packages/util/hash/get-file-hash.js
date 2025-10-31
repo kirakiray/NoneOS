4b63cf3696f433c817c67c42278e6531f82e23f5
huang yao <5945154+kirakiray@users.noreply.github.com>
2025-10-31 22:14:53 +0800

    refactor(util): 将哈希相关功能从fs模块迁移到util/hash目录

---
+++ packages/util/hash/get-file-hash.js
@@ -0,0 +1,13 @@
+import { calculateFileChunkHashes } from "./calculate-file-chunk-hashes.js";
+
+/**
+ * 获取文件的总体哈希值
+ * @param {File} file - 要计算哈希的文件对象
+ * @returns {Promise<string>} 返回文件的总体哈希值
+ * @description 将文件分割成128kb大小的块，计算每个块的SHA-256哈希值，
+ *             然后将所有块的哈希值拼接成字符串，最后计算这个字符串的哈希值作为文件的总体哈希值
+ */
+export const getFileHash = async (file) =>
+  getHash(
+    (await calculateFileChunkHashes(file, { chunkSize: 128 * 1024 })).join("")
+  );
---
+++ packages/util/hash/main.js
@@ -0,0 +1,3 @@
+export { getHash } from "./get-hash.js";
+export { calculateFileChunkHashes } from "./calculate-file-chunk-hashes.js";
+export { getFileHash } from "./get-file-hash.js";
