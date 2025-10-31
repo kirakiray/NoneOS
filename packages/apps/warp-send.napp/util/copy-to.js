317fd1fd132ede95ce877d7d29e94ea723ec096a
huang yao <5945154+kirakiray@users.noreply.github.com>
2025-10-31 16:01:45 +0800

    fix(warp-send): 修复文件传输中的取消逻辑和接收器初始化

--- packages/apps/warp-send.napp/util/copy-to.js
+++ packages/apps/warp-send.napp/util/copy-to.js
@@ -8,66 +8,87 @@ export const copyTo = ({
   fromUserId,
   formSessionId,
   progress,
+  end,
 }) => {
+  const controller = new AbortController();
+
+  const cancel = localUser.register("response-chunk", (e) => {
+    const { data, fromUserId, fromUserSessionId } = e.detail;
+
+    debugger;
+  });
+
   (async () => {
     // 获取远端用户
     const remoteUser = await localUser.connectUser(fromUserId);
 
     for (let file of files) {
       // 然后开始发送小块给对方
-      await sendFile({ file, formSessionId, remoteUser });
+      await sendFile({
+        signal: controller.signal,
+        file,
+        remoteUser,
+        userSessionId: formSessionId,
+      });
     }
   })();
+
+  return () => {
+    cancel();
+    controller.abort();
+  };
 };
 
-const sendFile = async ({ file, formSessionId, remoteUser }) => {
+// 发送文件给对方
+const sendFile = async ({ file, signal, remoteUser, userSessionId }) => {
   // 先给文件进行分块
   const chunkHashes = await calculateFileChunkHashes(file, {
     chunkSize: setting.chunkSize,
   });
 
-  // 组装数据，告诉对方项目的大小
-  const initData = {
+  const send = (data) => {
+    remoteUser.post(data, {
+      userSessionId,
+    });
+  };
+
+  // 组装数据，告诉对方文件的大小
+  send({
     kind: "send-file",
     name: file.name,
     chunkSize: setting.chunkSize,
     hashes: chunkHashes,
-  };
-
-  // 发送数据给对方
-  remoteUser.post(initData, {
-    sessionId: formSessionId,
   });
 
-  // 逐步分块发送给对方
+  // TODO: F逐步分块发送给对方
   for (let i = 0; i < chunkHashes.length; i++) {
-    const hash = chunkHashes[i];
+    // const hash = chunkHashes[i];
+
     let chunk = file.slice(i * setting.chunkSize, (i + 1) * setting.chunkSize);
-
-    const realHash = await getHash(chunk);
-
-    if (realHash !== hash) {
-      throw new Error("分块哈希不一致");
-    }
-
-    debugger;
 
     if (chunk instanceof Blob) {
       chunk = await chunk.arrayBuffer();
     }
 
-    remoteUser.post(new Uint8Array(chunk), {
-      kind: "post-chunk",
-      sessionId: formSessionId,
-      hash,
-    });
+    if (signal.aborted) {
+      break;
+    }
 
-    debugger;
+    // 发送块数据
+    send(new Uint8Array(chunk));
   }
 };
 
 // 初始化接收器
-export const initReceiver = ({ localUser, progress, handle }) => {
+export const initReceiver = async ({ localUser, progress, handle }) => {
+  const receiver = [];
+  let beforeReceiver = null;
+
+  // 存储块的目录
+  const chunksHandle = await handle.get("__warp-send-chunks", {
+    create: "dir",
+  });
+
   return localUser.bind("receive-data", async (e) => {
     const { data, fromUserId, fromUserSessionId } = e.detail;
 
@@ -80,6 +101,43 @@ export const initReceiver = ({ localUser, progress, handle }) => {
       return;
     }
 
-    debugger;
+    if (data.kind === "send-file") {
+      // 记录文件描述
+      beforeReceiver = {
+        kind: "send-file",
+        name: data.name, // 文件名称
+        hashes: data.hashes, // 文件的hash列表
+        receivedChunks: [], // 已经收到的块hash
+        chunkSize: data.chunkSize, // 每个块的最大大小
+      };
+
+      receiver.push(beforeReceiver);
+
+      // 收到了文件描述，开始准备接受文件
+      progress &&
+        progress({
+          ...beforeReceiver,
+        });
+      return;
+    }
+
+    if (data instanceof Uint8Array) {
+      // 收到了块文件，计算其hash
+      const hash = await getHash(data);
+
+      // 确认hash是否正确
+      const index = beforeReceiver.hashes.indexOf(hash);
+      if (index === -1) {
+        console.log("hash 不匹配");
+        return;
+      }
+
+      debugger;
+      // 先将块写入到缓存文件夹上
+
+      return;
+    }
+
+    // 不明情况
   });
 };
