ff5b51d7d2c6ba6f47a9a47ff474baac096f4d05
huang yao <5945154+kirakiray@users.noreply.github.com>
2026-01-01 15:44:42 +0800

    feat(fs): 添加挂载功能

--- packages/fs/handle/mount.js
+++ packages/fs/handle/mount.js
@@ -1,7 +1,5 @@
 import { DirHandle } from "./dir.js";
 
-export const mountedDirs = [];
-
 export const mount = async (options) => {
   if (!window.showDirectoryPicker) {
     throw new Error("showDirectoryPicker is not supported");
@@ -17,12 +15,69 @@ export const mount = async (options) => {
 
   const handle = new DirHandle(directoryHandle);
 
-  mountedDirs.push({
-    uniqueId: directoryHandle.getUniqueId
-      ? await directoryHandle.getUniqueId()
-      : Math.random().toString(36).slice(2),
-    handle,
-  });
+  if (options.save) {
+    saveHandle(handle._handle);
+  }
 
   return handle;
+};
+
+// 获取已经挂载的句柄列表
+export const getMounted = async () => {
+  const allHandles = await getAllHandles();
+
+  debugger;
+};
+
+// 保存db相关的操作
+// 数据库初始化（只在 keyPath 上加了 'id'）
+const dbPromise = new Promise((resolve) => {
+  const req = indexedDB.open("handles-db", 1);
+  req.onupgradeneeded = () =>
+    req.result.createObjectStore("handles", { keyPath: "id" });
+  req.onsuccess = () => resolve(req.result);
+});
+
+// 保存（自动生成唯一ID，返回ID值）
+const saveHandle = async (handle) => {
+  const db = await dbPromise;
+
+  let id;
+  if (handle.getUniqueId) {
+    id = await handle.getUniqueId();
+  } else {
+    // TODO: 判断已经保存的是否重复，isSameEntry判断
+    id = `${handle.kind}-${Date.now()}`;
+  }
+
+  await db.transaction("handles", "readwrite").objectStore("handles").put({
+    id,
+    name: handle.name,
+    handle,
+    time: Date.now(),
+  });
+
+  return id;
+};
+
+// // 加载指定ID的句柄
+// const loadHandle = async (id) => {
+//   const db = await dbPromise;
+//   return (await db.transaction("handles").objectStore("handles").get(id))
+//     ?.handle;
+// };
+
+// 获取所有句柄列表（含ID和名称）
+const getAllHandles = async () => {
+  const db = await dbPromise;
+  return db.transaction("handles").objectStore("handles").getAll();
+};
+
+// 删除指定ID
+const deleteHandle = async (id) => {
+  const db = await dbPromise;
+  await db
+    .transaction("handles", "readwrite")
+    .objectStore("handles")
+    .delete(id);
 };
--- packages/fs/main.js
+++ packages/fs/main.js
@@ -1,8 +1,8 @@
 export { init } from "./handle/main.js";
-export { mount } from "./handle/mount.js";
+export { mount, getMounted } from "./handle/mount.js";
 import { get as systemHandleGet } from "./handle/main.js";
-import { createGet } from "./fs-remote/main.js";
-import { createUser } from "../user/main.js";
+// import { createGet } from "./fs-remote/main.js";
+// import { createUser } from "../user/main.js";
 
 export const get = async (path, options) => {
   if (!path) {
@@ -20,6 +20,8 @@ export const get = async (path, options) => {
       userId = mark.split("-")[1];
     }
 
+    const { createUser } = await import("/packages/user/main.js");
+
     const localUser = await createUser();
     const remoteUser = await localUser.connectUser(userId);
 
@@ -31,6 +33,8 @@ export const get = async (path, options) => {
         console.log("initRTC error", error);
       }
     }
+
+    const { createGet } = await import("./fs-remote/main.js");
 
     // 远端用户的目录引用
     const remoteGet = createGet({ remoteUser });
